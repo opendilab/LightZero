a150b4f87e6bea2604b44fb27e46689c18f2ccf3
karroyan <karroyan@sjtu,edu,cn>
2022-11-02 11:38:14 +0800

    add gray config file and comments

--- zoo/atari/config/atari_efficientzero_gray_obs_config.py
+++ zoo/atari/config/atari_efficientzero_gray_obs_config.py
@@ -1,13 +1,9 @@
 import sys
 sys.path.append('/Users/puyuan/code/LightZero')
-# sys.path.append('/home/puyuan/LightZero')
-# sys.path.append('/mnt/nfs/puyuan/LightZero')
-# sys.path.append('/mnt/lustre/puyuan/LightZero')
 
 
 import torch
 from easydict import EasyDict
-
 from core.model import RepresentationNetwork
 
 if torch.cuda.is_available():
@@ -26,35 +22,22 @@ collector_env_num = 8
 n_episode = 8
 evaluator_env_num = 3
 
-# debug
-# collector_env_num = 1
-# n_episode = 1
-# evaluator_env_num = 1
-
 atari_efficientzero_config = dict(
-    # exp_name='data_ez_ctree/breakout_efficientzero_seed0_lr0.2_ns50_ftv025_upc1000_sub883',
-    exp_name='data_ez_ctree/pong_efficientzero_seed0_sub883_lr0.2_ns50_ftv025_upc1000_urv-false_cd-true',
+    exp_name='data_ez_ctree/pong_efficientzero_seed0_sub883_lr0.2_ns50_ftv025_upc1000_urv-false_cd-true-channel1',
     env=dict(
         collector_env_num=collector_env_num,
         evaluator_env_num=evaluator_env_num,
         n_evaluator_episode=evaluator_env_num,
         env_name='PongNoFrameskip-v4',
         stop_value=int(20),
-
-        # env_name='BreakoutNoFrameskip-v4',
-        # stop_value=int(1e6),
-
         collect_max_episode_steps=int(1.08e4),
         eval_max_episode_steps=int(1.08e5),
-        # for debug
-        # collect_max_episode_steps=int(100),
-        # eval_max_episode_steps=int(100),
         frame_skip=4,
-        obs_shape=(12, 96, 96),
+        # the first dimension of obs_shape should be image channels*stacks
+        obs_shape=(4, 96, 96),
         episode_life=True,
-        gray_scale=False,
-        # cvt_string=True,
-        # trade memory for speed
+        # whether to turn the RGB image to gray scale before encoder
+        gray_scale=True,
         cvt_string=False,
         game_wrapper=True,
         dqn_expert_data=False,
@@ -63,25 +46,18 @@ atari_efficientzero_config = dict(
     policy=dict(
         model_path=None,
         env_name='PongNoFrameskip-v4',
-        # env_name='BreakoutNoFrameskip-v4',
         # Whether to use cuda for network.
         cuda=True,
         model=dict(
             # whether to use discrete support to represent categorical distribution for value, reward/value_prefix
             categorical_distribution=True,
             representation_model_type='conv_res_blocks',
-            # representation_model=representation_model,
-            observation_shape=(12, 96, 96),  # 3,96,96 stack=4
+            observation_shape=(4, 96, 96),  # 1,96,96 stack=4
             action_space_size=6,  # for pong
-            # action_space_size=4,  # for breakout
             downsample=True,
             num_blocks=1,
-            # default config in EZ original repo
             num_channels=64,
             lstm_hidden_size=512,
-            # The env step is twice as large as the original size model when converging
-            # num_channels=32,
-            # lstm_hidden_size=256,
             reduced_channels_reward=16,
             reduced_channels_value=16,
             reduced_channels_policy=16,
@@ -99,10 +75,6 @@ atari_efficientzero_config = dict(
             state_norm=False,
         ),        # learn_mode config
         learn=dict(
-            # for debug
-            # update_per_collect=2,
-            # batch_size=4,
-
             update_per_collect=1000,
             batch_size=256,
 
@@ -118,8 +90,6 @@ atari_efficientzero_config = dict(
         ),
         # the eval cost is expensive, so we set eval_freq larger
         eval=dict(evaluator=dict(eval_freq=int(2e3), )),
-        # for debug
-        # eval=dict(evaluator=dict(eval_freq=int(2), )),
         # command_mode config
         other=dict(
             # NOTE: the replay_buffer_size is ineffective, we specify it in game config
@@ -132,85 +102,57 @@ atari_efficientzero_config = dict(
         device=device,
         # if mcts_ctree=True, using cpp mcts code
         mcts_ctree=True,
-        # mcts_ctree=False,
         image_based=True,
-        # cvt_string=True,
-        # trade memory for speed
         cvt_string=False,
         clip_reward=True,
         game_wrapper=True,
         # NOTE: different env have different action_space_size
         action_space_size=6,  # for pong
-        # action_space_size=4,  # for breakout
         amp_type='none',
-        obs_shape=(12, 96, 96),
-        image_channel=3,
-        gray_scale=False,
+        # the first dimension of obs_shape should be image channels*stacks
+        obs_shape=(4, 96, 96),
+        # image_channel should be set as 1 if grey_scale is true
+        image_channel=1,
+        # whether to turn the RGB image to gray scale before encoder
+        gray_scale=True,
         downsample=True,
         vis_result=True,
-        # TODO(pu): test the effect of augmentation
         use_augmentation=True,
         # Style of augmentation
         # choices=['none', 'rrc', 'affine', 'crop', 'blur', 'shift', 'intensity']
         augmentation=['shift', 'intensity'],
 
-        # for debug
-        # collector_env_num=1,
-        # evaluator_env_num=1,
-        # num_simulations=2,
-        # batch_size=4,
-        # game_history_length=20,
-        # total_transitions=int(1e2),
-        # lstm_hidden_size=32,
-        # td_steps=5,
-        # num_unroll_steps=5,
-        # lstm_horizon_len=5,
-
         collector_env_num=8,
         evaluator_env_num=3,
-        # TODO(pu): how to set proper num_simulations automatically?
         num_simulations=50,
         batch_size=256,
         game_history_length=400,
         total_transitions=int(1e5),
-        # default config in EZ original repo
         channels=64,
         lstm_hidden_size=512,
-        # The env step is twice as large as the original size model when converging
-        # channels=32,
-        # lstm_hidden_size=256,
         td_steps=5,
         num_unroll_steps=5,
         lstm_horizon_len=5,
 
-        # TODO(pu): why 0.99?
         revisit_policy_search_rate=0.99,
 
-        # TODO(pu): why not use adam?
         lr_manually=True,
 
-        # TODO(pu): if true, no priority to sample
         use_max_priority=True,  # if true, sample without priority
-        # use_max_priority=False,
         use_priority=True,
 
-        # TODO(pu): only used for adjust temperature manually
         max_training_steps=int(1e5),
         auto_temperature=False,
         # only effective when auto_temperature=False
         fixed_temperature_value=0.25,
-        # TODO(pu): whether to use root value in reanalyzing?
         use_root_value=False,
-        # use_root_value=True,
 
-        # TODO(pu): test the effect
         last_linear_layer_init_zero=True,
         state_norm=False,
         mini_infer_size=2,
         # (Float type) How much prioritization is used: 0 means no prioritization while 1 means full prioritization
         priority_prob_alpha=0.6,
         # (Float type)  How much correction is used: 0 means no correction while 1 means full correction
-        # TODO(pu): test effect of 0.4->1
         priority_prob_beta=0.4,
         prioritized_replay_eps=1e-6,
         root_dirichlet_alpha=0.3,
@@ -224,8 +166,6 @@ atari_efficientzero_config = dict(
         # whether to use discrete support to represent categorical distribution for value, reward/value_prefix
         categorical_distribution=True,
         support_size=300,
-        # value_support=DiscreteSupport(-300, 300, delta=1),
-        # reward_support=DiscreteSupport(-300, 300, delta=1),
         max_grad_norm=10,
         test_interval=10000,
         log_interval=1000,
@@ -245,7 +185,6 @@ atari_efficientzero_config = dict(
         # frame skip & stack observation
         frame_skip=4,
         frame_stack_num=4,
-        # TODO(pu): EfficientZero -> MuZero
         # coefficient
         reward_loss_coeff=1,
         value_loss_coeff=0.25,
@@ -278,7 +217,6 @@ atari_efficientzero_create_config = dict(
         type='atari_lightzero',
         import_names=['zoo.atari.envs.atari_lightzero_env'],
     ),
-    # env_manager=dict(type='base'),
     env_manager=dict(type='subprocess'),
     policy=dict(
         type='efficientzero',
