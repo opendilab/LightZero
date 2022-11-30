fix(pu): fix sampled ez using the sac-like log-prob when do tanh on the sampled actions

core/model/sampled_efficientzero/sampled_efficientzero_model.py
@@ -302,6 +302,7 @@ class PredictionNetwork(nn.Module):
             last_linear_layer_init_zero=False,
             sigma_type='fixed',
             fixed_sigma_value=0.3,
+            bound_type=None,
     ):
         """Prediction network
         Parameters
@@ -334,6 +335,7 @@ class PredictionNetwork(nn.Module):
         super().__init__()
         self.sigma_type = sigma_type
         self.fixed_sigma_value = fixed_sigma_value
+        self.bound_type = bound_type
         self.in_channels = in_channels
         if self.in_channels is not None:
             self.conv_input = nn.Conv2d(in_channels, num_channels, 1)
@@ -391,7 +393,7 @@ class PredictionNetwork(nn.Module):
             fixed_sigma_value=self.fixed_sigma_value,
             activation=nn.ReLU(),
             norm_type=None,
-            bound_type='tanh'  # TODO(pu)
+            bound_type=self.bound_type  # TODO(pu)
         )
 
         self.activation = nn.ReLU(inplace=True)
@@ -460,6 +462,7 @@ class SampledEfficientZeroNet(BaseNet):
             categorical_distribution=True,
             sigma_type='fixed',
             fixed_sigma_value=0.3,
+            bound_type=None,
     ):
         """
         Overview:
@@ -492,6 +495,8 @@ class SampledEfficientZeroNet(BaseNet):
         super(SampledEfficientZeroNet, self).__init__(lstm_hidden_size)
         self.sigma_type = sigma_type
         self.fixed_sigma_value = fixed_sigma_value
+        self.bound_type = bound_type
+
         self.categorical_distribution = categorical_distribution
         if not self.categorical_distribution:
             self.reward_support_size = 1
@@ -574,6 +579,7 @@ class SampledEfficientZeroNet(BaseNet):
                 last_linear_layer_init_zero=self.last_linear_layer_init_zero,
                 sigma_type=self.sigma_type,
                 fixed_sigma_value=self.fixed_sigma_value,
+                bound_type=self.bound_type,
             )
         else:
             if self.continuous_action_space:
@@ -618,6 +624,7 @@ class SampledEfficientZeroNet(BaseNet):
                 last_linear_layer_init_zero=self.last_linear_layer_init_zero,
                 sigma_type=self.sigma_type,
                 fixed_sigma_value=self.fixed_sigma_value,
+                bound_type=self.bound_type,
             )
 
         # projection

core/policy/sampled_efficientzero.py
@@ -170,7 +170,7 @@ class SampledEfficientZeroPolicy(Policy):
         # TODO(pu): priority
         inputs_batch, targets_batch, replay_buffer = data
 
-        obs_batch_ori, action_batch, child_actions_batch, mask_batch, indices, weights_lst, make_time = inputs_batch
+        obs_batch_ori, action_batch, child_sampled_actions_batch, mask_batch, indices, weights_lst, make_time = inputs_batch
         target_value_prefix, target_value, target_policy = targets_batch
 
         # [:, 0: config.frame_stack_num * 3,:,:]
@@ -214,7 +214,7 @@ class SampledEfficientZeroPolicy(Policy):
             obs_target_batch = self.transforms.transform(obs_target_batch)
 
         action_batch = torch.from_numpy(action_batch).to(self._cfg.device).unsqueeze(-1).long()
-        child_actions_batch = torch.from_numpy(child_actions_batch).to(self._cfg.device).unsqueeze(-1)
+        child_sampled_actions_batch = torch.from_numpy(child_sampled_actions_batch).to(self._cfg.device).unsqueeze(-1)
         mask_batch = torch.from_numpy(mask_batch).to(self._cfg.device).float()
         target_value_prefix = torch.from_numpy(target_value_prefix.astype('float64')).to(self._cfg.device
                                                                                          ).float()
@@ -319,30 +319,45 @@ class SampledEfficientZeroPolicy(Policy):
                                                                        - self._cfg.action_space_size:]
         dist = Independent(Normal(mu, sigma), 1)
 
-        target_policy_visit = target_policy[:, 0]
+        # take the init hypothetical step k=0
+        target_normalized_visit_count_init_step = target_policy[:, 0]
         # batch_size, num_unroll_steps, num_of_sampled_actions, action_dim, 1 -> batch_size, num_of_sampled_actions, action_dim
-        # 4, 3, 20, 2, 1 ->  4, 20, 2
-        target_policy_action = child_actions_batch[:, 0].squeeze(-1)
+        # e.g. 4, 6, 20, 2, 1 ->  4, 20, 2
+        target_sampled_actions = child_sampled_actions_batch[:, 0].squeeze(-1)
 
-
-        policy_entropy = dist.entropy().sum()
+        policy_entropy = dist.entropy().mean()
         entropy_loss = - policy_entropy
-
 
         # project the sampled-based improved policy back onto the space of representable policies
         # calculate KL loss
         # batch_size, num_of_sampled_actions -> 4,20
-        log_target = torch.log(target_policy_visit + 1e-9)
+        # target_normalized_visit_count_init_step is categorical distribution, the range of target_log_prob_sampled_actions is (-inf,0)
+        target_log_prob_sampled_actions = torch.log(target_normalized_visit_count_init_step + 1e-9)
         log_prob_sampled_actions = []
-        for i in range(self._cfg.num_of_sampled_actions):
-            # target_policy_action[:,i,:].shape: batch_size, action_dim -> 4,2
-            # dist.log_prob(target_policy_action[:,i,:]).shape: batch_size -> 4
-            log_prob = dist.log_prob(target_policy_action[:, i, :])
+        for k in range(self._cfg.num_of_sampled_actions):
+            # target_sampled_actions[:,i,:].shape: batch_size, action_dim -> 4,2
+            # dist.log_prob(target_sampled_actions[:,i,:]).shape: batch_size -> 4
+            # dist is normal distribution, the range of log_prob_sampled_actions is (-inf, inf)
+
+            # way 1:
+            # log_prob = dist.log_prob(target_sampled_actions[:, k, :])
+
+            # way 2:
+            y = 1 - target_sampled_actions[:, k, :].pow(2) + 1e-9
+            target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions[:, k, :])
+            # keep dimension for loss computation (usually for action space is 1 env. e.g. pendulum)
+            log_prob = dist.log_prob(target_sampled_actions_before_tanh).unsqueeze(-1)
+            log_prob = log_prob - torch.log(y).sum(-1, keepdim=True)
+            log_prob = log_prob.squeeze(-1)
+
             log_prob_sampled_actions.append(log_prob)
         # batch_size, num_of_sampled_actions -> 4,20
-        log_prob = torch.stack(log_prob_sampled_actions, dim=-1)
+        log_prob_sampled_actions = torch.stack(log_prob_sampled_actions, dim=-1)
 
-        policy_loss = (torch.exp(log_prob) * (log_prob - log_target)).sum()
+
+        # policy_loss = (torch.exp(log_prob_sampled_actions) * (log_prob_sampled_actions - target_log_prob_sampled_actions.detach())).sum(-1).mean(0)
+        policy_loss = (torch.exp(target_log_prob_sampled_actions.detach()) * (target_log_prob_sampled_actions.detach() - log_prob_sampled_actions)).sum(-1).mean(0)
+
         #############################
         # calculate policy loss: KL loss
         #############################
@@ -419,10 +434,11 @@ class SampledEfficientZeroPolicy(Policy):
                                                                            - self._cfg.action_space_size:]
             dist = Independent(Normal(mu, sigma), 1)
 
-            target_policy_visit = target_policy[:, step_i + 1]
+            # take the hypothetical step k>0
+            target_normalized_visit_count = target_policy[:, step_i + 1]
             # batch_size, num_unroll_steps, num_of_sampled_actions, action_dim, 1 -> batch_size, num_of_sampled_actions, action_dim
-            # 4,3, 20,2,1 ->  4, 20,2
-            target_policy_action = child_actions_batch[:, step_i + 1].squeeze(-1)
+            # e.g. 4, 3, 20, 2, 1 ->  4, 20, 2
+            target_sampled_actions = child_sampled_actions_batch[:, step_i + 1].squeeze(-1)
 
             policy_entropy = dist.entropy().mean()
             entropy_loss = - policy_entropy
@@ -430,17 +446,30 @@ class SampledEfficientZeroPolicy(Policy):
             # project the sampled-based improved policy back onto the space of representable policies
 
             # batch_size, num_of_sampled_actions -> 4,20
-            log_target = torch.log(target_policy_visit + 1e-9)
+            target_log_prob_sampled_actions = torch.log(target_normalized_visit_count + 1e-9)
             log_prob_sampled_actions = []
-            for i in range(self._cfg.num_of_sampled_actions):
-                # target_policy_action[:,i,:].shape: batch_size, action_dim -> 4,2
-                # dist.log_prob(target_policy_action[:,i,:]).shape: batch_size -> 4
-                log_prob = dist.log_prob(target_policy_action[:, i, :])
-                log_prob_sampled_actions.append(log_prob)
-            # batch_size, num_of_sampled_actions -> 4,20
-            log_prob = torch.stack(log_prob_sampled_actions, dim=-1)
+            for k in range(self._cfg.num_of_sampled_actions):
+                # target_sampled_actions[:,k,:].shape: (batch_size, action_dim) e.g. (4,2)
+                # dist.log_prob(target_sampled_actions[:,k,:]).shape: (batch_size,) e.g. (4,)
 
-            policy_loss = (torch.exp(log_prob) * (log_prob - log_target)).sum()  # KL divergence
+                # way 1:
+                # log_prob = dist.log_prob(target_sampled_actions[:, k, :])
+
+                # way 2:
+                y = 1 - target_sampled_actions[:, k, :].pow(2) + 1e-9
+                target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions[:, k, :])
+                # keep dimension for loss computation (usually for action space is 1 env. e.g. pendulum)
+                log_prob = dist.log_prob(target_sampled_actions_before_tanh).unsqueeze(-1)
+                log_prob = log_prob - torch.log(y).sum(-1, keepdim=True)
+                log_prob = log_prob.squeeze(-1)
+
+                log_prob_sampled_actions.append(log_prob)
+            # (batch_size, num_of_sampled_actions) e.g. (4,20)
+            log_prob_sampled_actions = torch.stack(log_prob_sampled_actions, dim=-1)
+
+            # policy_loss = (torch.exp(log_prob_sampled_actions) * (log_prob_sampled_actions - target_log_prob_sampled_actions.detach())).sum(-1).mean(0)  # KL divergence
+            policy_loss = (torch.exp(target_log_prob_sampled_actions.detach()) * (target_log_prob_sampled_actions.detach() - log_prob_sampled_actions)).sum(-1).mean(0)
+
             #############################
             # calculate policy loss: KL loss
             #############################
@@ -783,6 +812,7 @@ class SampledEfficientZeroPolicy(Policy):
             roots_distributions = roots.get_distributions()  # {list: 1}->{list:6}
             roots_sampled_actions = roots.get_sampled_actions()  # {list: 1}->{list:6}
 
+
             roots_values = roots.get_values()  # {list: 1}
             data_id = [i for i in range(active_collect_env_num)]
             output = {i: None for i in data_id}
@@ -799,6 +829,9 @@ class SampledEfficientZeroPolicy(Policy):
                     # print(error)
                     # print('ctree_sampled_efficientzero roots.get_sampled_actions() return list')
                     child_actions = np.array([action for action in roots_sampled_actions[i]])
+
+                if child_actions.max()>1 or child_actions.min()<-1:
+                    print('here')
 
                 # select the argmax, not sampling
                 # TODO(pu):

core/rl_utils/mcts/ptree_sampled_efficientzero/node.py
@@ -87,8 +87,18 @@ class Node:
         self.sigma = sigma
         dist = Independent(Normal(mu, sigma), 1)
         # print(dist.batch_shape, dist.event_shape)
-        sampled_actions = dist.sample(torch.tensor([self.num_of_sampled_actions]))
-        log_prob = dist.log_prob(sampled_actions)
+        sampled_actions_before_tanh = dist.sample(torch.tensor([self.num_of_sampled_actions]))
+
+        # way 1:
+        # log_prob = dist.log_prob(sampled_actions_before_tanh)
+
+        # way 2:
+        sampled_actions = torch.tanh(sampled_actions_before_tanh)
+        y = 1 - sampled_actions.pow(2) + 1e-6
+        # keep dimension for loss computation (usually for action space is 1 env. e.g. pendulum)
+        log_prob = dist.log_prob(sampled_actions_before_tanh).unsqueeze(-1)
+        log_prob = log_prob - torch.log(y).sum(-1, keepdim=True)
+
         # TODO: factored policy representation
         # empirical_distribution = [1/self.num_of_sampled_actions]
         for action_index in range(self.num_of_sampled_actions):
@@ -438,55 +448,56 @@ def select_child(
     # sampled related code
     ##################
     # Progressive widening (See https://hal.archives-ouvertes.fr/hal-00542673v2/document)
-    pw_alpha = 0.49
-    if len(root.children) < (root.visit_count + 1) ** pw_alpha:
-        distribution = torch.distributions.normal.Normal(root.mu, root.sigma)
-        sampled_action = distribution.sample().squeeze(0).detach().cpu().numpy()
+    # pw_alpha = 0.49
+    # TODO(pu)
+    # pw_alpha = -1
+    # if len(root.children) < (root.visit_count + 1) ** pw_alpha:
+    #     distribution = torch.distributions.normal.Normal(root.mu, root.sigma)
+    #     sampled_action = distribution.sample().squeeze(0).detach().cpu().numpy()
+    #
+    #     while Action(sampled_action) in root.children.keys():
+    #         # if sampled_action is sampled before, we sample a new action now
+    #         sampled_action = distribution.sample().squeeze(0).detach().cpu()
+    #     action = Action(sampled_action)
+    #
+    #     log_prob = distribution.log_prob(torch.tensor(sampled_action))
+    #     # TODO: factored policy representation
+    #     # empirical_distribution = [1/self.num_of_sampled_actions]
+    #
+    #     # pi, beta
+    #     root.children[action] = Node(prior=log_prob[0], legal_actions=None,
+    #                                  action_space_size=sampled_action.shape[0])  # TODO(pu): action_space_size
+    #     # TODO
+    #     # root.legal_actions.append(action)
+    #     # return action, root.children[action]
+    #     return action
+    #
+    # else:
+    max_score = -np.inf
+    epsilon = 0.000001
+    max_index_lst = []
+    for action, child in root.children.items():
+        ##################
+        # sampled related code
+        ##################
+        # use root as input argument
+        temp_score = compute_ucb_score(
+            root, child, min_max_stats, mean_q, root.is_reset, root.visit_count, root.value_prefix, pb_c_base,
+            pb_c_int,
+            discount, players
+        )
+        if max_score < temp_score:
+            max_score = temp_score
+            max_index_lst.clear()
+            max_index_lst.append(action)
+        elif temp_score >= max_score - epsilon:
+            # TODO(pu): if the difference is less than epsilon = 0.000001, we random choice action from  max_index_lst
+            max_index_lst.append(action)
 
-        while Action(sampled_action) in root.children.keys():
-            # if sampled_action is sampled before, we sample a new action now
-            sampled_action = distribution.sample().squeeze(0).detach().cpu().numpy()
-        action = Action(sampled_action)
+    if len(max_index_lst) > 0:
+        action = random.choice(max_index_lst)
 
-        log_prob = distribution.log_prob(sampled_action)
-        # TODO: factored policy representation
-        # empirical_distribution = [1/self.num_of_sampled_actions]
-
-        # pi, beta
-        root.children[action] = Node(prior=log_prob[0], legal_actions=None,
-                                     action_space_size=sampled_action.shape[0])  # TODO(pu): action_space_size
-        # TODO
-        # root.legal_actions.append(action)
-        # return action, root.children[action]
-        return action
-
-    else:
-
-        max_score = -np.inf
-        epsilon = 0.000001
-        max_index_lst = []
-        for action, child in root.children.items():
-            ##################
-            # sampled related code
-            ##################
-            # use root as input argument
-            temp_score = compute_ucb_score(
-                root, child, min_max_stats, mean_q, root.is_reset, root.visit_count, root.value_prefix, pb_c_base,
-                pb_c_int,
-                discount, players
-            )
-            if max_score < temp_score:
-                max_score = temp_score
-                max_index_lst.clear()
-                max_index_lst.append(action)
-            elif temp_score >= max_score - epsilon:
-                # TODO(pu): if the difference is less than epsilon = 0.000001, we random choice action from  max_index_lst
-                max_index_lst.append(action)
-
-        if len(max_index_lst) > 0:
-            action = random.choice(max_index_lst)
-
-        return action
+    return action
 
 
 def compute_ucb_score(
@@ -524,9 +535,9 @@ def compute_ucb_score(
     if node_prior == "uniform":
         prior_score = pb_c * (1 / len(parent.children))
     elif node_prior == "density":
-        # empirical distribution
+        # TODO(pu): empirical distribution
         prior_score = pb_c * (
-                child.prior / (sum([child.prior for child in parent.children.values()]) + 1e-9)
+                child.prior / (sum([node.prior for node in parent.children.values()]) + 1e-9)
         )
     else:
         raise ValueError("{} is unknown prior option, choose uniform or density")

zoo/classic_control/pendulum/config/pendulum_sampled_efficientzero_config.py
@@ -10,8 +10,8 @@ os.environ['DISABLE_MUJOCO_RENDERING'] = '1'
 import sys
 
 # sys.path.append('/Users/puyuan/code/LightZero')
-# sys.path.append('/home/puyuan/LightZero')
-sys.path.append('/mnt/nfs/puyuan/LightZero')
+sys.path.append('/home/puyuan/LightZero')
+# sys.path.append('/mnt/nfs/puyuan/LightZero')
 # sys.path.append('/mnt/lustre/puyuan/LightZero')
 
 import torch
@@ -22,21 +22,23 @@ if torch.cuda.is_available():
 else:
     device = 'cpu'
 
-collector_env_num = 8
-n_episode = 8
-evaluator_env_num = 3
-batch_size = 256
-K = 5  # action_space_size=1
-num_simulations = 25  # action_space_size=1
-
+# collector_env_num = 8
+# n_episode = 8
+# evaluator_env_num = 3
+# batch_size = 256
+# K = 5  # action_space_size=1
+# num_simulations = 25  # action_space_size=1
+# update_per_collect = 250
 
 # for debug
-# collector_env_num = 1
-# n_episode = 1
-# evaluator_env_num = 1
-# batch_size = 4
-# K = 5  # action_space_size=1
-# num_simulations = 6  # action_space_size=1
+collector_env_num = 1
+n_episode = 1
+evaluator_env_num = 1
+batch_size = 32
+K = 3  # action_space_size=1
+num_simulations = 10  # action_space_size=1
+update_per_collect = 50
+
 
 categorical_distribution = True
 
@@ -44,7 +46,7 @@ categorical_distribution = True
 # The key hyper-para to tune, for different env, we have different episode_length
 # e.g. reuse_factor = 0.5
 # we usually set update_per_collect = collector_env_num * episode_length * reuse_factor
-update_per_collect = 250
+
 game_history_length = 50
 observation_dim = 3
 action_dim = 1
@@ -73,6 +75,7 @@ pendulum_sampled_efficientzero_config = dict(
         model=dict(
             sigma_type='fixed',  # conditioned
             fixed_sigma_value=0.3,
+            bound_type=None,
             # activation=torch.nn.ReLU(inplace=True),
             # whether to use discrete support to represent categorical distribution for value, reward/value_prefix
             categorical_distribution=categorical_distribution,
@@ -165,8 +168,8 @@ pendulum_sampled_efficientzero_config = dict(
         ######################################
         env_type='no_board_games',
         device=device,
-        # mcts_ctree=False,
-        mcts_ctree=True,
+        mcts_ctree=False,
+        # mcts_ctree=True,
         battle_mode='one_player_mode',
         game_history_length=game_history_length,
         action_space_size=action_dim,  # 4**2