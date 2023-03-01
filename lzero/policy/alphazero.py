fix(pu,zt): fix numerical stability bug in sez arctanh(), fix alphazero current_state_scale bug

lzero/policy/alphazero.py
@@ -274,10 +274,10 @@ class AlphaZeroPolicy(Policy):
                 action and the score of the env state
         """
         legal_actions = env.legal_actions
-        current_state = env.current_state()
-        current_state = torch.from_numpy(current_state).to(device=self._device, dtype=torch.float).unsqueeze(0)
+        current_state, current_state_scale = env.current_state()
+        current_state_scale = torch.from_numpy(current_state_scale).to(device=self._device, dtype=torch.float).unsqueeze(0)
         with torch.no_grad():
-            action_probs, value = self._policy_model.compute_prob_value(current_state)
+            action_probs, value = self._policy_model.compute_prob_value(current_state_scale)
         action_probs_dict = dict(zip(legal_actions, action_probs.squeeze(0)[legal_actions].detach().cpu().numpy()))
         return action_probs_dict, value.item()
 

lzero/policy/sampled_efficientzero.py
@@ -493,9 +493,13 @@ class SampledEfficientZeroPolicy(Policy):
 
                 # way 2: SAC-like
                 y = 1 - target_sampled_actions[:, k, :].pow(2)
-                target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions[:, k, :])
+                # for numerical stability
+                target_sampled_actions_clamped = torch.clamp(target_sampled_actions[:, k, :], torch.tensor(-1 + 1e-6),
+                                                             torch.tensor(1 - 1e-6))
+                target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions_clamped)
+
                 # keep dimension for loss computation (usually for action space is 1 env. e.g. pendulum)
-                log_prob = dist.log_prob(target_sampled_actions_before_tanh + 1e-6).unsqueeze(-1)
+                log_prob = dist.log_prob(target_sampled_actions_before_tanh).unsqueeze(-1)
                 log_prob = log_prob - torch.log(y + 1e-6).sum(-1, keepdim=True)
                 log_prob = log_prob.squeeze(-1)
 
@@ -696,9 +700,13 @@ class SampledEfficientZeroPolicy(Policy):
 
                     # way 2:
                     y = 1 - target_sampled_actions[:, k, :].pow(2)
-                    target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions[:, k, :])
+                    # for numerical stability
+                    target_sampled_actions_clamped = torch.clamp(target_sampled_actions[:, k, :],
+                                                                 torch.tensor(-1 + 1e-6),
+                                                                 torch.tensor(1 - 1e-6))
+                    target_sampled_actions_before_tanh = torch.arctanh(target_sampled_actions_clamped)
                     # keep dimension for loss computation (usually for action space is 1 env. e.g. pendulum)
-                    log_prob = dist.log_prob(target_sampled_actions_before_tanh + 1e-6).unsqueeze(-1)
+                    log_prob = dist.log_prob(target_sampled_actions_before_tanh).unsqueeze(-1)
 
                     log_prob = log_prob - torch.log(y + 1e-6).sum(-1, keepdim=True)
                     log_prob = log_prob.squeeze(-1)