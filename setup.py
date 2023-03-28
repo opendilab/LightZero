0bc54a9b2cf7c9d639e7c3a1e5e2ad096cc565ab
puyuan1996 <2402552459@qq.com>
2023-03-28 13:24:53 +0800

    fix(pu): replace -std=c++11 with /std:c++11

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ def find_cython_extensions(path=None):
         extensions.append(Extension(
             extname, [item],
             include_dirs=[np.get_include()],
-            extra_compile_args=["-std=c++11"],
-            extra_link_args=["-std=c++11"],
+            extra_compile_args=["/std:c++11"],
+            extra_link_args=["/std:c++11"],
         ))
 
     return extensions
