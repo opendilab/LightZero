901c6583fcecb0e01b348bf5c32ae1b05ffbf452
蒲源 <2402552459@qq.com>
2023-06-08 17:48:50 +0800

    fix(pu): fix gen{gumbel_rng} compatibility, fix mcts_ptree_sampled value/value_prefix

--- lzero/worker/__init__.py
+++ lzero/worker/__init__.py
@@ -2,4 +2,3 @@ from .alphazero_collector import AlphaZeroCollector
 from .alphazero_evaluator import AlphaZeroEvaluator
 from .muzero_collector import MuZeroCollector
 from .muzero_evaluator import MuZeroEvaluator
-from .gumbel_muzero_collector import GumbelMuZeroCollector
