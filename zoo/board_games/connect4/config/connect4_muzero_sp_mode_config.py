3ffbd0769e09e17f52cbbe3c5f9c1f574e019a2e
HarryXuancy <lhsxxcy@126.com>
2023-10-13 14:48:34 +0800

    polish(xcy):add muzero sp mode config

--- zoo/board_games/connect4/config/connect4_muzero_sp_mode_config.py
+++ zoo/board_games/connect4/config/connect4_muzero_sp_mode_config.py
@@ -8,17 +8,20 @@ n_episode = 8
 evaluator_env_num = 5
 num_simulations = 50
 update_per_collect = 50
+reanalyze_ratio = 0.
 batch_size = 256
 max_env_step = int(5e5)
-model_path = None
 # ==============================================================
 # end of the most frequently changed config specified by the user
 # ==============================================================
-connect4_alphazero_config = dict(
-    exp_name='data_az_ptree/connect4_bot-mode_seed0',
+
+connect4_muzero_config = dict(
+    exp_name=
+    f'data_mz_ctree/connect4_spmode_rulebot_seed0',
     env=dict(
-        battle_mode='play_with_bot_mode',
+        battle_mode='self_play_mode',
         bot_action_type='rule',
+        channel_last=True,
         collector_env_num=collector_env_num,
         evaluator_env_num=evaluator_env_num,
         n_evaluator_episode=evaluator_env_num,
@@ -28,52 +31,53 @@ connect4_alphazero_config = dict(
         model=dict(
             observation_shape=(3, 6, 7),
             action_space_size=7,
+            image_channel=3,
             num_res_blocks=1,
             num_channels=64,
+            support_scale=300,
+            reward_support_size=601,
+            value_support_size=601,
         ),
         cuda=True,
         env_type='board_games',
+        game_segment_length=int(6 * 7 / 2),  # for battle_mode='play_with_bot_mode'
         update_per_collect=update_per_collect,
         batch_size=batch_size,
         optim_type='Adam',
         lr_piecewise_constant_decay=False,
         learning_rate=0.003,
         grad_clip_value=0.5,
-        value_weight=1.0,
-        entropy_weight=0.0,
+        num_simulations=num_simulations,
+        reanalyze_ratio=reanalyze_ratio,
+        # NOTE：In board_games, we set large td_steps to make sure the value target is the final outcome.
+        td_steps=int(6 * 7 / 2),  # for battle_mode='play_with_bot_mode'
+        # NOTE：In board_games, we set discount_factor=1.
+        discount_factor=1,
         n_episode=n_episode,
         eval_freq=int(2e3),
-        mcts=dict(num_simulations=num_simulations),
+        replay_buffer_size=int(1e5),
         collector_env_num=collector_env_num,
         evaluator_env_num=evaluator_env_num,
     ),
 )
+connect4_muzero_config = EasyDict(connect4_muzero_config)
+main_config = connect4_muzero_config
 
-connect4_alphazero_config = EasyDict(connect4_alphazero_config)
-main_config = connect4_alphazero_config
-
-connect4_alphazero_create_config = dict(
+connect4_muzero_create_config = dict(
     env=dict(
         type='connect4',
         import_names=['zoo.board_games.connect4.envs.connect4_env'],
     ),
     env_manager=dict(type='subprocess'),
     policy=dict(
-        type='alphazero',
-        import_names=['lzero.policy.alphazero'],
+        type='muzero',
+        import_names=['lzero.policy.muzero'],
     ),
-    collector=dict(
-        type='episode_alphazero',
-        import_names=['lzero.worker.alphazero_collector'],
-    ),
-    evaluator=dict(
-        type='alphazero',
-        import_names=['lzero.worker.alphazero_evaluator'],
-    )
 )
-connect4_alphazero_create_config = EasyDict(connect4_alphazero_create_config)
-create_config = connect4_alphazero_create_config
+connect4_muzero_create_config = EasyDict(connect4_muzero_create_config)
+create_config = connect4_muzero_create_config
 
-if __name__ == '__main__':
-    from lzero.entry import train_alphazero
-    train_alphazero([main_config, create_config], seed=0, model_path=model_path, max_env_step=max_env_step)
+if __name__ == "__main__":
+    from lzero.entry import train_muzero
+
+    train_muzero([main_config, create_config], seed=1, max_env_step=max_env_step)
