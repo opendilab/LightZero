4de2a9ec30c76e0daa43147d0287ae070d3ee3ac
蒲源 <2402552459@qq.com>
2023-10-27 01:51:41 +0800

    fix(pu): fix emplace_back in macOS

--- lzero/mcts/ctree/ctree_sampled_efficientzero/lib/cnode.cpp
+++ lzero/mcts/ctree/ctree_sampled_efficientzero/lib/cnode.cpp
@@ -380,7 +380,11 @@ namespace tree
             // After sorting, the first vector is the index, and the second vector is the probability value after perturbation sorted from large to small.
             for (size_t iter = 0; iter < disturbed_probs.size(); iter++)
             {
-                disc_action_with_probs.emplace_back(std::make_pair(iter, disturbed_probs[iter]));
+                #ifdef __APPLE__
+                    disc_action_with_probs.__emplace_back(std::make_pair(iter, disturbed_probs[iter]));
+                #else
+                    disc_action_with_probs.emplace_back(std::make_pair(iter, disturbed_probs[iter]));
+                #endif
             }
 
             std::sort(disc_action_with_probs.begin(), disc_action_with_probs.end(), cmp);
