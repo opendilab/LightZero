b63b693b14655f7d2ac5c42e26ef39360baba4ed
HarryXuancy <lhsxxcy@126.com>
2023-11-28 16:53:16 +0800

    feature(xcy): add pong config

--- zoo/atari/config/ma_pong1_config.py
+++ zoo/atari/config/ma_pong1_config.py
@@ -1,7 +1,7 @@
 from easydict import EasyDict
 
 # options={'PongNoFrameskip-v4', 'QbertNoFrameskip-v4', 'MsPacmanNoFrameskip-v4', 'SpaceInvadersNoFrameskip-v4', 'BreakoutNoFrameskip-v4', ...}
-env_name = 'MsPacmanNoFrameskip-v4'
+env_name = 'PongNoFrameskip-v4'
 
 if env_name == 'PongNoFrameskip-v4':
     action_space_size = 6
@@ -21,12 +21,10 @@ collector_env_num = 8
 n_episode = 8
 evaluator_env_num = 3
 num_simulations = 50
-update_per_collect = None
+update_per_collect = 1000
 batch_size = 256
-model_update_ratio = 0.25
-max_env_step = int(2e6)
+max_env_step = int(8e5)
 reanalyze_ratio = 1.0
-
 eps_greedy_exploration_in_collect = False
 # ==============================================================
 # end of the most frequently changed config specified by the user
@@ -34,7 +32,7 @@ eps_greedy_exploration_in_collect = False
 
 atari_muzero_config = dict(
     exp_name=
-    f'data_mz_ctree/{env_name[:-14]}/reuse_mctsvalue_ratio1',
+    f'data_mz_ctree/{env_name[:-14]}/ma1',
     env=dict(
         stop_value=int(1e6),
         env_name=env_name,
@@ -69,7 +67,6 @@ atari_muzero_config = dict(
         ),
         use_augmentation=True,
         update_per_collect=update_per_collect,
-        model_update_ratio=model_update_ratio,
         batch_size=batch_size,
         optim_type='SGD',
         lr_piecewise_constant_decay=True,
@@ -107,4 +104,9 @@ create_config = atari_muzero_create_config
 
 if __name__ == "__main__":
     from lzero.entry import train_ma
-    train_ma([main_config, create_config], seed=1, max_env_step=max_env_step)
+    # train_ma([main_config, create_config], seed=0, max_env_step=max_env_step)
+    # 下面为cprofile的代码
+    def run(max_env_step: int):
+        train_ma([main_config, create_config], seed=0, max_env_step=max_env_step)
+    import cProfile
+    cProfile.run(f"run({30000})", filename="pong_ma_cprofile_2k_envstep", sort="cumulative")
