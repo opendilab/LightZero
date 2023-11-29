de89e54653ae173052403ac13b6b3b7c0eb3a37d
nighood <nighoodren@gmail.com>
2023-11-29 15:37:47 +0800

    fix(rjy): fix mamujoco and add test

--- zoo/multiagent_mujoco/envs/multiagent_mujoco_lightzero_env.py
+++ zoo/multiagent_mujoco/envs/multiagent_mujoco_lightzero_env.py
@@ -7,24 +7,18 @@ from ding.envs import BaseEnvTimestep
 from ding.envs.common import save_frames_as_gif
 from ding.torch_utils import to_ndarray
 from ding.utils import ENV_REGISTRY
-from dizoo.multiagent_mujoco.envs.multi_mujoco_env import MujocoEnv
+from dizoo.multiagent_mujoco.envs.multi_mujoco_env import MujocoEnv,MujocoMulti
 
 
 @ENV_REGISTRY.register('mujoco_lightzero')
 class MAMujocoEnvLZ(MujocoEnv):
     """
     Overview:
-        The modified MuJoCo environment with continuous action space for LightZero's algorithms.
+        The modified Multi-agentMuJoCo environment with continuous action space for LightZero's algorithms.
     """
 
     config = dict(
         stop_value=int(1e6),
-        action_clip=False,
-        delay_reward_step=0,
-        replay_path=None,
-        save_replay_gif=False,
-        replay_path_gif=None,
-        action_bins_per_branch=None,
         norm_obs=dict(use_norm=False, ),
         norm_reward=dict(use_norm=False, ),
     )
@@ -34,40 +28,50 @@ class MAMujocoEnvLZ(MujocoEnv):
         self._cfg = cfg
         # We use env_name to indicate the env_id in LightZero.
         self._cfg.env_id = self._cfg.env_name
-        self._action_clip = cfg.action_clip
-        self._delay_reward_step = cfg.delay_reward_step
         self._init_flag = False
-        self._replay_path = None
-        self._replay_path_gif = cfg.replay_path_gif
-        self._save_replay_gif = cfg.save_replay_gif
-        self._action_bins_per_branch = cfg.action_bins_per_branch
 
     def reset(self) -> np.ndarray:
         if not self._init_flag:
-            self._env = self._make_env()
-            if self._replay_path is not None:
-                self._env = gym.wrappers.RecordVideo(
-                    self._env,
-                    video_folder=self._replay_path,
-                    episode_trigger=lambda episode_id: True,
-                    name_prefix='rl-video-{}'.format(id(self))
-                )
-
-            self._env.observation_space.dtype = np.float32
-            self._observation_space = self._env.observation_space
-            self._action_space = self._env.action_space
-            self._reward_space = gym.spaces.Box(
-                low=self._env.reward_range[0], high=self._env.reward_range[1], shape=(1,), dtype=np.float32
-            )
+            self._env = MujocoMulti(env_args=self._cfg)
             self._init_flag = True
+            
         if hasattr(self, '_seed') and hasattr(self, '_dynamic_seed') and self._dynamic_seed:
             np_seed = 100 * np.random.randint(1, 1000)
             self._env.seed(self._seed + np_seed)
         elif hasattr(self, '_seed'):
             self._env.seed(self._seed)
+        
         obs = self._env.reset()
-        obs = to_ndarray(obs).astype('float32')
+        obs = to_ndarray(obs)
         self._eval_episode_return = 0.
+        self.env_info = self._env.get_env_info()
+
+        self._num_agents = self.env_info['n_agents']
+        self._agents = [i for i in range(self._num_agents)]
+        self._observation_space = gym.spaces.Dict(
+            {
+                'agent_state': gym.spaces.Box(
+                    low=float("-inf"), high=float("inf"), shape=obs['agent_state'].shape, dtype=np.float32
+                ),
+                'global_state': gym.spaces.Box(
+                    low=float("-inf"), high=float("inf"), shape=obs['global_state'].shape, dtype=np.float32
+                ),
+            }
+        )
+        self._action_space = gym.spaces.Dict({agent: self._env.action_space[agent] for agent in self._agents})
+        single_agent_obs_space = self._env.action_space[self._agents[0]]
+        if isinstance(single_agent_obs_space, gym.spaces.Box):
+            self._action_dim = single_agent_obs_space.shape
+        elif isinstance(single_agent_obs_space, gym.spaces.Discrete):
+            self._action_dim = (single_agent_obs_space.n, )
+        else:
+            raise Exception('Only support `Box` or `Discrte` obs space for single agent.')
+        self._reward_space = gym.spaces.Dict(
+            {
+                agent: gym.spaces.Box(low=float("-inf"), high=float("inf"), shape=(1, ), dtype=np.float32)
+                for agent in self._agents
+            }
+        )
 
         action_mask = None
         obs = {'observation': obs, 'action_mask': action_mask, 'to_play': -1}
@@ -75,25 +79,13 @@ class MAMujocoEnvLZ(MujocoEnv):
         return obs
 
     def step(self, action: Union[np.ndarray, list]) -> BaseEnvTimestep:
-        if self._action_bins_per_branch:
-            action = self.map_action(action)
         action = to_ndarray(action)
-        if self._save_replay_gif:
-            self._frames.append(self._env.render(mode='rgb_array'))
-        if self._action_clip:
-            action = np.clip(action, -1, 1)
         obs, rew, done, info = self._env.step(action)
         self._eval_episode_return += rew
         if done:
-            if self._save_replay_gif:
-                path = os.path.join(
-                    self._replay_path_gif, '{}_episode_{}.gif'.format(self._cfg.env_name, self._save_replay_count)
-                )
-                save_frames_as_gif(self._frames, path)
-                self._save_replay_count += 1
             info['eval_episode_return'] = self._eval_episode_return
 
-        obs = to_ndarray(obs).astype(np.float32)
+        obs = to_ndarray(obs)
         rew = to_ndarray([rew]).astype(np.float32)
 
         action_mask = None
