48f40024bfdc141cc9afbf6795e3a89c57a3f17d
puyuan1996 <2402552459@qq.com>
2023-12-22 00:29:26 +0800

    polish(pu): polish configs

--- lzero/entry/train_muzero_gpt.py
+++ lzero/entry/train_muzero_gpt.py
@@ -174,7 +174,9 @@ def train_muzero_gpt(
 
         # if collect_cnt % 3 == 0:
         # if collect_cnt % 1 == 0:
-        if learner.train_iter <=  int(3e4) :  # 30K fixed
+        # if learner.train_iter <=  int(1e5) :  # 100K fixed
+        if learner.train_iter <=  int(5e4) :  # 50K fixed
+
 
             # TODO: 0, 2, 4 更新
             replay_buffer._cfg.num_unroll_steps = 1
--- lzero/model/gpt_models/cfg_atari.py
+++ lzero/model/gpt_models/cfg_atari.py
@@ -1,79 +1,28 @@
 # atari orignet
-cfg = {}
-cfg['tokenizer'] = {'_target_': 'models.tokenizer.Tokenizer',
-                    'vocab_size': 512, # TODO: for atari
-                    'embed_dim': 512,
-                    # 'vocab_size': 256,  # TODO: for atari debug
-                    # 'embed_dim': 256,
-                    # 'vocab_size': 128,  # TODO: for atari debug
-                    # 'embed_dim': 128,
-                    # 'encoder':
-                    #            {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
-                    #             'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-                    #             'out_ch': 3, 'dropout': 0.0},# TODO：for atari debug
-                    #  'decoder':
-                    #       {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
-                    #       'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-                    #       'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari debug
-                    'encoder':
-                    {'resolution': 64, 'in_channels': 3, 'z_channels': 512, 'ch': 64,
-                                'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-                                'out_ch': 3, 'dropout': 0.0},# TODO：for atari
-                            'decoder':
-                    {'resolution': 64, 'in_channels': 3, 'z_channels': 512, 'ch': 64,
-                     'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-                     'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari
-cfg['world_model'] = {
-                        'tokens_per_block': 17,
-                      # 'max_blocks': 20,
-                      #   "max_tokens": 17 * 20,  # TODO： horizon
-                      'max_blocks': 5,
-                      "max_tokens": 17 * 5,  # TODO： horizon
-                      'attention': 'causal',
-                      # 'num_layers': 10,# TODO：for atari
-                      'num_layers': 2, # TODO：for atari debug
-                      'num_heads': 4,
-                      # 'embed_dim': 128, # TODO：for atari
-                      'embed_dim': 256, # TODO：for atari
-                      # 'embed_dim': 64, # TODO：for atari debug
-                      'embed_pdrop': 0.1,
-                      'resid_pdrop': 0.1,
-                      'attn_pdrop': 0.1,
-                      "device": 'cuda:2',
-                    #   "device": 'cpu',
-                      # 'support_size': 601,
-                      'support_size': 21,
-                      'action_shape': 6,# TODO：for atari
-                      'max_cache_size':500,
-                      "env_num":8,
-                      }
-from easydict import EasyDict
-cfg = EasyDict(cfg)
-
-# # mediumnet
 # cfg = {}
 # cfg['tokenizer'] = {'_target_': 'models.tokenizer.Tokenizer',
-#                     # 'vocab_size': 512, # TODO: for atari
-#                     # 'embed_dim': 512,
+#                     'vocab_size': 512, # TODO: for atari
+#                     'embed_dim': 512,
 #                     # 'vocab_size': 256,  # TODO: for atari debug
 #                     # 'embed_dim': 256,
-#                     'vocab_size': 128,  # TODO: for atari debug
-#                     'embed_dim': 128,
-#                     'encoder':
-#                                {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
-#                                 'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-#                                 'out_ch': 3, 'dropout': 0.0},# TODO：for atari debug
-#                             'decoder':
-#                     {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
-#                      'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-#                      'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari debug
-#                     # {'resolution': 64, 'in_channels': 1, 'z_channels': 512, 'ch': 64,
+#                     # 'vocab_size': 128,  # TODO: for atari debug
+#                     # 'embed_dim': 128,
+#                     # 'encoder':
+#                     #            {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
 #                     #             'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-#                     #             'out_ch': 3, 'dropout': 0.0},# TODO：for atari
-#                     #         'decoder':
-#                     # {'resolution': 64, 'in_channels': 1, 'z_channels': 512, 'ch': 64,
-#                     #  'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
-#                     #  'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari
+#                     #             'out_ch': 3, 'dropout': 0.0},# TODO：for atari debug
+#                     #  'decoder':
+#                     #       {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
+#                     #       'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                     #       'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari debug
+#                     'encoder':
+#                     {'resolution': 64, 'in_channels': 3, 'z_channels': 512, 'ch': 64,
+#                                 'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                                 'out_ch': 3, 'dropout': 0.0},# TODO：for atari
+#                             'decoder':
+#                     {'resolution': 64, 'in_channels': 3, 'z_channels': 512, 'ch': 64,
+#                      'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                      'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari
 # cfg['world_model'] = {
 #                         'tokens_per_block': 17,
 #                       # 'max_blocks': 20,
@@ -84,38 +33,38 @@ cfg = EasyDict(cfg)
 #                       # 'num_layers': 10,# TODO：for atari
 #                       'num_layers': 2, # TODO：for atari debug
 #                       'num_heads': 4,
-#                       'embed_dim': 128, # TODO：for atari
+#                       # 'embed_dim': 128, # TODO：for atari
+#                       'embed_dim': 256, # TODO：for atari
 #                       # 'embed_dim': 64, # TODO：for atari debug
 #                       'embed_pdrop': 0.1,
 #                       'resid_pdrop': 0.1,
 #                       'attn_pdrop': 0.1,
-#                       "device": 'cuda:3',
+#                       "device": 'cuda:1',
 #                     #   "device": 'cpu',
-#                       'support_size': 601,
+#                       # 'support_size': 601,
+#                       'support_size': 21,
 #                       'action_shape': 6,# TODO：for atari
-#                       'max_cache_size':5000,
+#                       'max_cache_size':500,
 #                       "env_num":8,
 #                       }
 # from easydict import EasyDict
 # cfg = EasyDict(cfg)
 
-# small net
+# # mediumnet
 cfg = {}
 cfg['tokenizer'] = {'_target_': 'models.tokenizer.Tokenizer',
                     # 'vocab_size': 512, # TODO: for atari
                     # 'embed_dim': 512,
                     # 'vocab_size': 256,  # TODO: for atari debug
                     # 'embed_dim': 256,
-                    # 'vocab_size': 128,  # TODO: for atari debug
-                    # 'embed_dim': 128,
-                       'vocab_size': 64,  # TODO: for atari debug
-                    'embed_dim': 64,
+                    'vocab_size': 128,  # TODO: for atari debug
+                    'embed_dim': 128,
                     'encoder':
-                               {'resolution': 64, 'in_channels': 3, 'z_channels': 64, 'ch': 64,
+                               {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
                                 'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
                                 'out_ch': 3, 'dropout': 0.0},# TODO：for atari debug
                             'decoder':
-                    {'resolution': 64, 'in_channels': 3, 'z_channels': 64, 'ch': 64,
+                    {'resolution': 64, 'in_channels': 3, 'z_channels': 128, 'ch': 64,
                      'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
                      'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari debug
                     # {'resolution': 64, 'in_channels': 1, 'z_channels': 512, 'ch': 64,
@@ -135,8 +84,8 @@ cfg['world_model'] = {
                       # 'num_layers': 10,# TODO：for atari
                       'num_layers': 2, # TODO：for atari debug
                       'num_heads': 4,
-                      # 'embed_dim': 128, # TODO：for atari
-                      'embed_dim': 64, # TODO：for atari debug
+                      'embed_dim': 128, # TODO：for atari
+                      # 'embed_dim': 64, # TODO：for atari debug
                       'embed_pdrop': 0.1,
                       'resid_pdrop': 0.1,
                       'attn_pdrop': 0.1,
@@ -149,3 +98,54 @@ cfg['world_model'] = {
                       }
 from easydict import EasyDict
 cfg = EasyDict(cfg)
+
+# small net
+# cfg = {}
+# cfg['tokenizer'] = {'_target_': 'models.tokenizer.Tokenizer',
+#                     # 'vocab_size': 512, # TODO: for atari
+#                     # 'embed_dim': 512,
+#                     # 'vocab_size': 256,  # TODO: for atari debug
+#                     # 'embed_dim': 256,
+#                     # 'vocab_size': 128,  # TODO: for atari debug
+#                     # 'embed_dim': 128,
+#                        'vocab_size': 64,  # TODO: for atari debug
+#                     'embed_dim': 64,
+#                     'encoder':
+#                                {'resolution': 64, 'in_channels': 3, 'z_channels': 64, 'ch': 64,
+#                                 'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                                 'out_ch': 3, 'dropout': 0.0},# TODO：for atari debug
+#                             'decoder':
+#                     {'resolution': 64, 'in_channels': 3, 'z_channels': 64, 'ch': 64,
+#                      'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                      'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari debug
+#                     # {'resolution': 64, 'in_channels': 1, 'z_channels': 512, 'ch': 64,
+#                     #             'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                     #             'out_ch': 3, 'dropout': 0.0},# TODO：for atari
+#                     #         'decoder':
+#                     # {'resolution': 64, 'in_channels': 1, 'z_channels': 512, 'ch': 64,
+#                     #  'ch_mult': [1, 1, 1, 1, 1], 'num_res_blocks': 2, 'attn_resolutions': [8, 16],
+#                     #  'out_ch': 3, 'dropout': 0.0}}  # TODO：for atari
+# cfg['world_model'] = {
+#                         'tokens_per_block': 17,
+#                       # 'max_blocks': 20,
+#                       #   "max_tokens": 17 * 20,  # TODO： horizon
+#                       'max_blocks': 5,
+#                       "max_tokens": 17 * 5,  # TODO： horizon
+#                       'attention': 'causal',
+#                       # 'num_layers': 10,# TODO：for atari
+#                       'num_layers': 2, # TODO：for atari debug
+#                       'num_heads': 4,
+#                       # 'embed_dim': 128, # TODO：for atari
+#                       'embed_dim': 64, # TODO：for atari debug
+#                       'embed_pdrop': 0.1,
+#                       'resid_pdrop': 0.1,
+#                       'attn_pdrop': 0.1,
+#                       "device": 'cuda:3',
+#                     #   "device": 'cpu',
+#                       'support_size': 601,
+#                       'action_shape': 6,# TODO：for atari
+#                       'max_cache_size':5000,
+#                       "env_num":8,
+#                       }
+# from easydict import EasyDict
+# cfg = EasyDict(cfg)
