1c8b92b7786b4e4783ad79f7de4d9a8902b91479
dyyoungg <yangdeyu@sensetime.com>
2024-09-20 17:45:23 +0800

    polish(pu): polish uz configs

--- zoo/atari/config/atari_unizero_sgement_config.py
+++ zoo/atari/config/atari_unizero_sgement_config.py
@@ -42,16 +42,18 @@ batch_size = 64
 num_unroll_steps = 10
 infer_context_length = 4
 
+num_layers = 2
+
 # ====== only for debug =====
-# collector_env_num = 8
-# num_segments = 8
-# evaluator_env_num = 2
-# num_simulations = 5
-# max_env_step = int(2e5)
-# reanalyze_ratio = 0.1
-# batch_size = 64
-# num_unroll_steps = 10
-# replay_ratio = 0.01
+collector_env_num = 8
+num_segments = 8
+evaluator_env_num = 2
+num_simulations = 5
+max_env_step = int(2e5)
+reanalyze_ratio = 0.
+batch_size = 64
+num_unroll_steps = 10
+replay_ratio = 0.05
 
 # ==============================================================
 # end of the most frequently changed config specified by the user
@@ -69,18 +71,18 @@ atari_unizero_config = dict(
         n_evaluator_episode=evaluator_env_num,
         manager=dict(shared_memory=False, ),
         # TODO: only for debug
-        # collect_max_episode_steps=int(20),
-        # eval_max_episode_steps=int(20),
+        collect_max_episode_steps=int(20),
+        eval_max_episode_steps=int(20),
     ),
     policy=dict(
-        learn=dict(learner=dict(hook=dict(save_ckpt_after_iter=20000,),),),  # default is 10000
+        learn=dict(learner=dict(hook=dict(save_ckpt_after_iter=100000,),),),  # default is 10000
         model=dict(
             # observation_shape=(3, 64, 64),
             observation_shape=(3, 96, 96),
             action_space_size=action_space_size,
             world_model_cfg=dict(
-                # policy_entropy_weight=0,  # NOTE
-                policy_entropy_weight=1e-4,
+                policy_entropy_weight=0,  # NOTE
+                # policy_entropy_weight=1e-4,
                 continuous_action_space=False,
                 max_blocks=num_unroll_steps,
                 max_tokens=2 * num_unroll_steps,  # NOTE: each timestep has 2 tokens: obs and action
@@ -88,7 +90,7 @@ atari_unizero_config = dict(
                 device='cuda',
                 # device='cpu',
                 action_space_size=action_space_size,
-                num_layers=2,
+                num_layers=num_layers,
                 num_heads=8,
                 embed_dim=768,
                 obs_type='image',
@@ -101,7 +103,8 @@ atari_unizero_config = dict(
         # use_augmentation=True,
         use_augmentation=False,
 
-        manual_temperature_decay=True,  # TODO
+        # manual_temperature_decay=True,  # TODO
+        manual_temperature_decay=False,  # TODO
         threshold_training_steps_for_final_temperature=int(2.5e4),
         # manual_temperature_decay=False,  # TODO
 
@@ -113,8 +116,8 @@ atari_unizero_config = dict(
         replay_ratio=replay_ratio,
         batch_size=batch_size,
         optim_type='AdamW',
-        learning_rate=0.0001,
-        # learning_rate=0.001,  # TODO
+        # learning_rate=0.0001,
+        learning_rate=0.1,  # TODO
         num_simulations=num_simulations,
         reanalyze_ratio=reanalyze_ratio,
         num_segments=num_segments,
@@ -156,10 +159,10 @@ if __name__ == "__main__":
     seeds = [0]  # You can add more seed values here
     for seed in seeds:
         # Update exp_name to include the current seed
-        main_config.exp_name = f'data_efficiency0829_plus_tune-uz_0918/{env_id[:-14]}/{env_id[:-14]}_uz_numsegments-{num_segments}_gsl{game_segment_length}_pew1e-4_decaytemp25k_upc{update_per_collect}-rr{replay_ratio}_rer{reanalyze_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}'
+        # main_config.exp_name = f'data_efficiency0829_plus_tune-uz_0920/{env_id[:-14]}/{env_id[:-14]}_uz_nlayer{num_layers}_numsegments-{num_segments}_gsl{game_segment_length}_temp025_upc{update_per_collect}-rr{replay_ratio}_rer{reanalyze_ratio}_H{num_unroll_steps}-infer{infer_context_length}_bs{batch_size}_seed{seed}'
         # main_config.exp_name = f'data_efficiency0829_plus_tune-uz_0917/numsegments-{num_segments}_gsl{game_segment_length}_origin-target-value-policy_pew0_fixsample_temp025_useprio/{env_id[:-14]}_stack1_unizero_upc{update_per_collect}-rr{replay_ratio}_rer{reanalyze_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}_nlayer2'
 
-        # main_config.exp_name = f'data_efficiency0829_plus_tune-uz_debug/numsegments-{num_segments}_gsl{game_segment_length}_fix/obshape96_use-augmentation-obsw10/{env_id[:-14]}_stack1_unizero_upc{update_per_collect}-rr{replay_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}_nlayer2'
+        main_config.exp_name = f'data_efficiency0829_plus_tune-uz_debug/numsegments-{num_segments}_gsl{game_segment_length}_fix/obshape96_use-augmentation-obsw10/{env_id[:-14]}_stack1_unizero_upc{update_per_collect}-rr{replay_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}_nlayer2'
 
         from lzero.entry import train_unizero
         train_unizero([main_config, create_config], seed=seed, model_path=main_config.policy.model_path, max_env_step=max_env_step)
