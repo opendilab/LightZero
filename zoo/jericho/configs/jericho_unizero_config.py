060c815bba8bd0c2727ea139cb08436196a7071d
‘whl’ <18231213@buaa.edu.cn>
2024-12-01 10:31:53 +0800

    init commit

---
+++ zoo/jericho/configs/jericho_unizero_config.py
@@ -0,0 +1,103 @@
+from easydict import EasyDict
+
+
+def main(env_id='zork1.z5', seed=0):
+    action_space_size = 50
+
+    # ==============================================================
+    # begin of the most frequently changed config specified by the user
+    # ==============================================================
+    collector_env_num = 8
+    game_segment_length = 20
+    evaluator_env_num = 5
+    num_simulations = 50
+    max_env_step = int(5e5)
+    batch_size = 64
+    num_unroll_steps = 10
+    infer_context_length = 4
+    num_layers = 2
+    replay_ratio = 0.25
+    # ==============================================================
+    # end of the most frequently changed config specified by the user
+    # ==============================================================
+    jericho_unizero_config = dict(
+        env=dict(
+            stop_value=int(1e6),
+            max_action_num=action_space_size,
+            tokenizer_path="google-bert/bert-base-uncased",
+            max_seq_len=512,
+            game_path="z-machine-games-master/jericho-game-suite/" + env_id,
+            collector_env_num=collector_env_num,
+            evaluator_env_num=evaluator_env_num,
+            n_evaluator_episode=evaluator_env_num,
+            manager=dict(shared_memory=False, )
+        ),
+        policy=dict(
+            # default is 10000
+            learn=dict(learner=dict(
+                hook=dict(save_ckpt_after_iter=1000000, ), ), ),
+            model=dict(
+                observation_shape=512,
+                action_space_size=action_space_size,
+                world_model_cfg=dict(
+                    policy_entropy_weight=5e-3,
+                    continuous_action_space=False,
+                    max_blocks=num_unroll_steps,
+                    # NOTE: each timestep has 2 tokens: obs and action
+                    max_tokens=2 * num_unroll_steps,
+                    context_length=2 * infer_context_length,
+                    device='cuda',
+                    action_space_size=action_space_size,
+                    num_layers=num_layers,
+                    num_heads=8,
+                    embed_dim=768,
+                    obs_type='vector',  # TODO: Change it.
+                    env_num=max(collector_env_num, evaluator_env_num),
+                ),
+            ),
+            model_path=None,
+            num_unroll_steps=num_unroll_steps,
+            replay_ratio=replay_ratio,
+            batch_size=batch_size,
+            learning_rate=0.0001,
+            num_simulations=num_simulations,
+            train_start_after_envsteps=2000,
+            game_segment_length=game_segment_length,
+            replay_buffer_size=int(1e6),
+            eval_freq=int(5e3),
+            collector_env_num=collector_env_num,
+            evaluator_env_num=evaluator_env_num,
+        ),
+    )
+    jericho_unizero_config = EasyDict(jericho_unizero_config)
+
+    jericho_unizero_create_config = dict(
+        env=dict(
+            type='jericho_lightzero',
+            import_names=['zoo.jericho.envs.jericho_env'],
+        ),
+        env_manager=dict(type='subprocess'),
+        policy=dict(
+            type='unizero',
+            import_names=['lzero.policy.unizero'],
+        ),
+    )
+    jericho_unizero_create_config = EasyDict(jericho_unizero_create_config)
+    main_config = jericho_unizero_config
+    create_config = jericho_unizero_create_config
+
+    main_config.exp_name = f'data_unizero/{env_id[:-14]}/{env_id[:-14]}_uz_nlayer{num_layers}_gsl{game_segment_length}_rr{replay_ratio}_Htrain{num_unroll_steps}-Hinfer{infer_context_length}_bs{batch_size}_seed{seed}'
+    from lzero.entry import train_unizero
+    train_unizero([main_config, create_config], seed=seed,
+                  model_path=main_config.policy.model_path, max_env_step=max_env_step)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some environment.')
+    parser.add_argument('--env', type=str,
+                        help='The environment to use', default='zork1.z5')
+    parser.add_argument('--seed', type=int, help='The seed to use', default=0)
+    args = parser.parse_args()
+
+    main(args.env, args.seed)
---
+++ zoo/jericho/envs/jericho_env.py
@@ -0,0 +1,112 @@
+import copy
+from typing import List
+
+import gym
+import numpy as np
+from transformers import AutoTokenizer
+from ding.utils import ENV_REGISTRY
+from ding.envs import BaseEnv, BaseEnvTimestep
+from jericho import FrotzEnv
+
+
+@ENV_REGISTRY.register('jericho')
+class JerichoEnv(BaseEnv):
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.game_path = cfg.game_path
+        self.max_action_num = cfg.max_action_num
+        self.max_seq_len = cfg.max_seq_len
+        self.tokenizer = AutoTokenizer.from_pretrained(cfg.tokenizer_path)
+
+        self._env = FrotzEnv(self.game_path)
+        self._action_list = None
+
+        self.finished = False
+        self._init_flag = False
+        self.episode_return = 0
+
+        self.observation_space = gym.spaces.Dict()
+        self.action_space = gym.spaces.Discrete(self.max_action_num)
+        self.reward_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(1, ), dtype=np.float32)
+    
+    def prepare_obs(self, obs, return_str: bool = False):
+        if self._action_list is None:
+            self._action_list = self._env.get_valid_actions()
+        full_obs = obs + "\nValid actions: " + str(self._action_list)
+        if not return_str:
+            full_obs = self.tokenizer([full_obs], truncation=True, padding=True, max_length=self.max_seq_len)
+            full_obs = np.array(full_obs['input_ids'][0], dtype=np.int32)
+        action_mask = [1] * len(self._action_list) + [0] * (self.max_action_num - len(self._action_list))
+        action_mask = np.array(action_mask, dtype=np.int8)
+        return {'observation': full_obs, 'action_mask': action_mask, 'to_play': -1}
+    
+    def reset(self, return_str: bool = False):
+        initial_observation, info = self._env.reset()
+        self.episode_return = 0
+        return self.prepare_obs(initial_observation, return_str)
+
+    def seed(self, seed: int, dynamic_seed: bool = True) -> None:
+        """
+        Set the seed for the environment.
+        """
+        self._seed = seed
+        self._env.seed(seed)
+
+    def close(self) -> None:
+        self._init_flag = False
+
+    def __repr__(self) -> str:
+        return "LightZero Jericho Env"
+
+    def step(self, action: int, return_str: bool = False):
+        action_str = self._action_list[action]
+        observation, reward, done, info = self._env.step(action_str)
+        self.episode_return += reward
+        self._action_list = None
+        observation = self.prepare_obs(observation, return_str)
+
+        if done:
+            self.finished = True
+            info['eval_episode_return'] = self.episode_return
+
+        return BaseEnvTimestep(observation, reward, done, info)
+    
+    @staticmethod
+    def create_collector_env_cfg(cfg: dict) -> List[dict]:
+        collector_env_num = cfg.pop('collector_env_num')
+        cfg = copy.deepcopy(cfg)
+        # when in collect phase, sometimes we need to normalize the reward
+        # reward_normalize is determined by the config.
+        cfg.is_collect = True
+        return [cfg for _ in range(collector_env_num)]
+
+    @staticmethod
+    def create_evaluator_env_cfg(cfg: dict) -> List[dict]:
+        evaluator_env_num = cfg.pop('evaluator_env_num')
+        cfg = copy.deepcopy(cfg)
+        # when in evaluate phase, we don't need to normalize the reward.
+        cfg.reward_normalize = False
+        cfg.is_collect = False
+        return [cfg for _ in range(evaluator_env_num)]
+
+
+if __name__ == '__main__':
+    from easydict import EasyDict
+    env_cfg = EasyDict(
+        dict(
+            game_path="z-machine-games-master/jericho-game-suite/zork1.z5",
+            max_action_num=50,
+            tokenizer_path="google-bert/bert-base-uncased",
+            max_seq_len=512
+        )
+    )
+    env = JerichoEnv(env_cfg)
+    obs = env.reset(return_str=True)
+    print(f'[OBS]:\n{obs["observation"]}')
+    while True:
+        action_id = int(input('Please input the action id:'))
+        obs, reward, done, info = env.step(action_id, return_str=True)
+        print(f'[OBS]:\n{obs["observation"]}')
+        if done:
+            break
