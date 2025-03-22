97004c81dfd4894a700077d87ef480edff2ad752
xiongjyu <xiongjyu@gmail.com>
2025-03-22 17:56:52 +0800

    commit only the changes to the jericho_env file

--- zoo/jericho/envs/jericho_env.py
+++ zoo/jericho/envs/jericho_env.py
@@ -10,6 +10,7 @@ from ding.utils import ENV_REGISTRY, set_pkg_seed, get_rank, get_world_size
 from ding.envs import BaseEnv, BaseEnvTimestep
 from jericho import FrotzEnv
 import os
+import json
 from datetime import datetime
 
 
@@ -32,8 +33,7 @@ class JerichoEnv(BaseEnv):
         - for_unizero (:obj:`bool`): If True, specify additional keys for unizero compatibility.
         - save_replay (:obj:`bool`): If True, the interaction log of the entire episode will be saved. 
         - save_replay_path (:obj:`str`): Path where interaction logs are saved.
-        - collect_policy_mode (:obj:`str`): Data collection strategies, including "human" and "random".
-        - n_collector_episodes (:obj:`int`): The total number of episodes of data collected by the data collectors.
+        - collect_policy_mode (:obj:`str`): Data collection strategies, including "human", "random" and "expert".
         - env_type (:obj:`str`): Type of environment.
     Attributes:
         - tokenizer (Optional[AutoTokenizer]): The tokenizer loaded from the pretrained model.
@@ -52,8 +52,7 @@ class JerichoEnv(BaseEnv):
         'save_replay': False,
         'save_replay_path': None,
         'env_type': "zork1",
-        'collect_policy_mode': "random",
-        'n_collector_episodes': 1
+        'collect_policy_mode': "random"
     }
 
     def __init__(self, cfg: Dict[str, Any]) -> None:
@@ -75,7 +74,6 @@ class JerichoEnv(BaseEnv):
         self.save_replay: bool = self.cfg['save_replay']
         self.save_replay_path: str = self.cfg['save_replay_path']
         self.collect_policy_mode: str = self.cfg['collect_policy_mode']
-        self.n_collector_episodes: int = self.cfg['n_collector_episodes']
         self.env_type: str = self.cfg['env_type']
 
         # Record the last observation and action for detecting stuck actions.
@@ -109,8 +107,8 @@ class JerichoEnv(BaseEnv):
         self.episode_return: float = 0.0
         self.env_step: int = 0
         self.timestep: int = 0
-        self.episode_history: List[Dict[str, Any]] = []
-        self.collected_episodes_experiences: List[List[Dict[str, Any]]] = []
+        self.episode_history: Optional[List[Dict[str, Any]]] = None
+        self.walkthrough_actions: Optional[List[str]] = None
 
 
         # Define observation, action, and reward spaces.
@@ -202,6 +200,7 @@ class JerichoEnv(BaseEnv):
         self.env_step = 0
         self.timestep = 0
         self.episode_history = []
+        self.walkthrough_actions = self._env.get_walkthrough()
 
         if self.remove_stuck_actions:
             self.last_observation = initial_observation
@@ -212,8 +211,15 @@ class JerichoEnv(BaseEnv):
         self.rank = get_rank()
 
         processed_obs = self.prepare_obs(initial_observation, return_str)
-        self.episode_history.append({'observation': processed_obs['observation'], 'info': info, 'done': False})
 
+        self.episode_history.append({
+            'timestep': 0,
+            'obs': processed_obs['observation'],
+            'act': None,
+            'done': False,
+            'info': info
+        })
+       
         return processed_obs
 
     def seed(self, seed: int, dynamic_seed: bool = True) -> None:
@@ -310,12 +316,12 @@ class JerichoEnv(BaseEnv):
             done = True
 
         self.episode_history.append({
-            "step": self.env_step,
-            "observation": processed_obs['observation'],
-            "action": action_str,
-            "reward": reward.item() if isinstance(reward, np.ndarray) else reward,
-            "done": done,
-            "info": info
+            'timestep': self.timestep,
+            'obs': processed_obs['observation'],
+            'act': action_str,
+            'reward': reward.item() if isinstance(reward, np.ndarray) else reward,
+            'done': done,
+            'info': info
         })
 
         if done:
@@ -323,6 +329,7 @@ class JerichoEnv(BaseEnv):
             print(f'rank {self.rank} one episode done!')
             self.finished = True
             info['eval_episode_return'] = self.episode_return
+
             if self.save_replay:
                 self.save_episode_data()
 
@@ -372,23 +379,15 @@ class JerichoEnv(BaseEnv):
         os.makedirs(self.save_replay_path, exist_ok=True)
 
         timestamp = datetime.now().strftime("%m%d_%H%M")
-        filename = os.path.join(self.save_replay_path, f"episode_record_{self.env_type}_{timestamp}.txt")
-        try:
-            with open(filename, mode="w", encoding="utf-8") as f:
-                for i, item in enumerate(self.episode_history):
-                    item['observation'] = item['observation'].strip('\n')
-                    if i == 0:  # Starting Status
-                        f.write(f"[ENV]\tmoves: {item['info']['moves']}\t\tscore: {item['info']['score']}\n{item['observation']}\n\n")
-                    else:
-                        f.write(f"[PLAYER]\tstep: {item['step']}\naction: {item['action']}\nreward: {item['reward']}\n\n")
-                        f.write(f"[ENV]\tmoves: {item['info']['moves']}\t\tscore: {item['info']['score']}\n{item['observation']}\n\n")
-                    if item['done']:
-                        f.write(f"[SYSTEM]\tThe game is over, and the reward for this game is {self.episode_return}.\n")
-                        
-            print(f"[INFO] Episode saved to {filename}")
-        except Exception as e:
-            print(f"[ERROR] Failed to save episode: {e}")
+        filename = os.path.join(self.save_replay_path, f"episode_record_{self.env_type}_{self.collect_policy_mode}_{timestamp}.json")
+        
+        info = self.episode_history[-1]['info']
+        if 'eval_episode_return' in info and isinstance(info['eval_episode_return'], np.ndarray):
+            info['eval_episode_return'] = info['eval_episode_return'].item()
 
+        with open(filename, mode="w", encoding="utf-8") as f:
+            json.dump(self.episode_history, f, ensure_ascii=False)
+         
     def human_step(self, observation:str) -> str:
         """
         Get action input from human player.
@@ -415,45 +414,34 @@ class JerichoEnv(BaseEnv):
 
     def collect_episode_data(self):
         """
-        Collects episode data for the specified number of episodes.
+        Collect data for an episode
         """
 
-        for _ in range(self.n_collector_episodes):
-            obs = self.reset(return_str=True)
-                
-            done = False
-            state = obs['observation']
-            episode_experiences = []
+        obs = self.reset(return_str=True)
 
-            while not done:
-                if self.collect_policy_mode == 'human':
-                    action = self.human_step(state)
-                elif self.collect_policy_mode == 'random':
-                    action = self.random_step()
-                else:
-                    raise ValueError(f"Invalid collect_policy_mode: {self.collect_policy_mode}")
+        done = False
+        expert_step_count = 0
 
-                next_obs, reward, done, info = self.step(action, return_str=True)
-                next_state = next_obs['observation']
+        while not done:
+            if self.collect_policy_mode == 'human':
+                action = self.human_step(obs['observation'])
+            elif self.collect_policy_mode == 'random':
+                action = self.random_step()
+            elif self.collect_policy_mode == 'expert':
+                action = self.walkthrough_actions[expert_step_count]
+                expert_step_count += 1
+            else:
+                raise ValueError(f"Invalid collect_policy_mode: {self.collect_policy_mode}")
 
-                experience = {
-                    'state': state,
-                    'action': action,
-                    'reward': reward.item() if isinstance(reward, np.ndarray) else reward,
-                    'next_state': next_state,
-                    'done': done
-                }
-                episode_experiences.append(experience)
+            obs, reward, done, info = self.step(action, return_str=True)
 
-                state = next_state
-                
-                if done:
-                    self.collected_episodes_experiences.append(episode_experiences)
-                    print(f'The game is over, and the reward for this game is {self.episode_return}.\n')
-                    break
-                    
-        # print(f'collected_episodes_experiences={self.collected_episodes_experiences}')
+            if self.collect_policy_mode == 'expert' and expert_step_count >= len(self.walkthrough_actions):
+                done = True
 
+            if done:
+                info['eval_episode_return'] = self.episode_return
+                break
+     
 if __name__ == '__main__':
     from easydict import EasyDict
 
@@ -472,25 +460,11 @@ if __name__ == '__main__':
             evaluator_env_num=1,
             save_replay=True,
             save_replay_path=None,
-            env_type='zork1',        # zork1, acorncourt, detective, omniquest
-            collect_policy_mode='random',
-            n_collector_episodes=1
+            env_type='zork1',               # zork1, acorncourt, detective, omniquest
+            collect_policy_mode='expert'    # random, human, expert
         )
     )
     env = JerichoEnv(env_cfg)
+    # Collect data for an episode according to collect_policy_mode
     env.collect_episode_data()  
-
-    obs = env.reset(return_str=True)
-    print(f'[OBS]:\n{obs["observation"]}')
-    while True:
-        try:
-            action_id = int(input('Please input the action id (the id starts from zero): '))
-        except ValueError:
-            print("Invalid input. Please enter an integer action id.")
-            continue
-        obs, reward, done, info = env.step(action_id, return_str=True)
-        print(f'[OBS]:\n{obs["observation"]}')
-        if done:
-            user_choice = input('Would you like to RESTART, RESTORE a saved game, give the FULL score for that game or QUIT? ')
-            del env  
-            break
+    del env  
