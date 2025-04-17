867dc6ff89db5e01090e383a5101071ba30ecdfb
xiongjyu <xiongjyu@gmail.com>
2025-04-17 15:38:20 +0800

    feature(xjy): add decode text regularization option and related logs (#348)

--- lzero/policy/unizero.py
+++ lzero/policy/unizero.py
@@ -662,7 +662,8 @@ class UniZeroPolicy(MuZeroPolicy):
                 roots = MCTSPtree.roots(active_collect_env_num, legal_actions)
 
             roots.prepare(self._cfg.root_noise_weight, noises, reward_roots, policy_logits, to_play)
-            self._mcts_collect.search(roots, self._collect_model, latent_state_roots, to_play, timestep)
+
+            next_latent_state_with_env = self._mcts_collect.search(roots, self._collect_model, latent_state_roots, to_play, timestep)
 
             # list of list, shape: ``{list: batch_size} -> {list: action_space_size}``
             roots_visit_count_distributions = roots.get_distributions()
@@ -690,6 +691,9 @@ class UniZeroPolicy(MuZeroPolicy):
                     # NOTE: Convert the ``action_index_in_legal_action_set`` to the corresponding ``action`` in the entire action set.
                     action = np.where(action_mask[i] == 1.0)[0][action_index_in_legal_action_set]
 
+                next_latent_state = next_latent_state_with_env[env_id][action]
+                predicted_ids = self._collect_model.tokenizer.decode_to_language_logits_for_inference(        embeddings=next_latent_state, max_length=512,  pad_token_id=0, eos_token_id=102)
+
                 # ============== TODO: only for visualize ==============
                 # action_index_in_legal_action_set, visit_count_distribution_entropy = select_action(
                 #     distributions, temperature=self._collect_mcts_temperature, deterministic=True
@@ -704,7 +708,8 @@ class UniZeroPolicy(MuZeroPolicy):
                     'searched_value': value,
                     'predicted_value': pred_values[i],
                     'predicted_policy_logits': policy_logits[i],
-                    'timestep': timestep[i]
+                    'timestep': timestep[i],
+                    'predicted_next_ids': predicted_ids,
                 }
                 batch_action.append(action)
 
@@ -788,14 +793,14 @@ class UniZeroPolicy(MuZeroPolicy):
                 # python mcts_tree
                 roots = MCTSPtree.roots(active_eval_env_num, legal_actions)
             roots.prepare_no_noise(reward_roots, policy_logits, to_play)
-            self._mcts_eval.search(roots, self._eval_model, latent_state_roots, to_play, timestep)
+            next_latent_state_with_env = self._mcts_eval.search(roots, self._eval_model, latent_state_roots, to_play, timestep)
 
             # list of list, shape: ``{list: batch_size} -> {list: action_space_size}``
             roots_visit_count_distributions = roots.get_distributions()
             roots_values = roots.get_values()  # shape: {list: batch_size}
 
             batch_action = []
-
+            
             for i, env_id in enumerate(ready_env_id):
                 distributions, value = roots_visit_count_distributions[i], roots_values[i]
                 # print("roots_visit_count_distributions:", distributions, "root_value:", value)
@@ -811,6 +816,12 @@ class UniZeroPolicy(MuZeroPolicy):
                 # entire action set.
                 action = np.where(action_mask[i] == 1.0)[0][action_index_in_legal_action_set]
 
+                # 通过选中的action和policy预测得到下一个latent state
+                next_latent_state = next_latent_state_with_env[env_id][action]
+                predicted_ids = self._eval_model.tokenizer.decode_to_language_logits_for_inference( embeddings=next_latent_state,
+                                                                                                    max_length=512,
+                                                                                                    pad_token_id=0,
+                                                                                                    eos_token_id=102)
                 output[env_id] = {
                     'action': action,
                     'visit_count_distributions': distributions,
@@ -818,7 +829,8 @@ class UniZeroPolicy(MuZeroPolicy):
                     'searched_value': value,
                     'predicted_value': pred_values[i],
                     'predicted_policy_logits': policy_logits[i],
-                    'timestep': timestep[i]
+                    'timestep': timestep[i],
+                    'predicted_next_ids': predicted_ids,
                 }
                 batch_action.append(action)
 
