polish(pu): fix norm score computation, adapt config to aliyun

lzero/model/unizero_model.py
@@ -98,30 +98,33 @@ class UniZeroModel(nn.Module):
             print(f'{sum(p.numel() for p in self.tokenizer.encoder.parameters())} parameters in agent.tokenizer.encoder')
             print('==' * 20)
         elif world_model_cfg.obs_type == 'image':
-            # self.representation_network = RepresentationNetworkUniZero(
-            #     observation_shape,
-            #     num_res_blocks,
-            #     num_channels,
-            #     self.downsample,
-            #     activation=self.activation,
-            #     norm_type=norm_type,
-            #     embedding_dim=world_model_cfg.embed_dim,
-            #     group_size=world_model_cfg.group_size,
-            #     final_norm_option_in_encoder=world_model_cfg.final_norm_option_in_encoder,
-            # )
-
-            self.representation_network = ViT(
-                image_size =observation_shape[1],
-                # patch_size = 32,
-                patch_size = 8,
-                num_classes = world_model_cfg.embed_dim,
-                dim = 768,
-                depth = 12,
-                heads = 12,
-                mlp_dim = 3072,
-                dropout = 0.1,
-                emb_dropout = 0.1
-            )
+            if world_model_cfg.encoder_type == "resnet":
+                self.representation_network = RepresentationNetworkUniZero(
+                    observation_shape,
+                    num_res_blocks,
+                    num_channels,
+                    self.downsample,
+                    activation=self.activation,
+                    norm_type=norm_type,
+                    embedding_dim=world_model_cfg.embed_dim,
+                    group_size=world_model_cfg.group_size,
+                    final_norm_option_in_encoder=world_model_cfg.final_norm_option_in_encoder,
+                )
+            elif world_model_cfg.encoder_type == "vit":
+                # vit base
+                self.representation_network = ViT(
+                    image_size =observation_shape[1],
+                    # patch_size = 32,
+                    patch_size = 8,
+                    num_classes = world_model_cfg.embed_dim,
+                    dim = 768,
+                    depth = 12,
+                    heads = 12,
+                    mlp_dim = 3072,
+                    dropout = 0.1,
+                    emb_dropout = 0.1,
+                    final_norm_option_in_encoder=world_model_cfg.final_norm_option_in_encoder,
+                )
 
             # ====== for analysis ======
             if world_model_cfg.analysis_sim_norm:

lzero/model/vit.py
@@ -109,7 +109,7 @@ class ViT(nn.Module):
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
         self.pool = pool
-        self.to_latent = nn.Identity()
+        # self.to_latent = nn.Identity()
 
         # self.mlp_head = nn.Linear(dim, num_classes)
         self.last_linear = nn.Linear(dim, num_classes)
@@ -139,7 +139,7 @@ class ViT(nn.Module):
 
         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
 
-        x = self.to_latent(x)
+        # x = self.to_latent(x)
         
         # x = self.mlp_head(x)
         x = self.last_linear(x)

lzero/policy/unizero_multitask.py
@@ -17,7 +17,7 @@ from lzero.policy.unizero import UniZeroPolicy
 from .utils import configure_optimizers_nanogpt
 import sys
 
-sys.path.append('/fs-computility/ai-shen/puyuan/code/LibMTL')
+sys.path.append('/cpfs04/user/puyuan/code/LibMTL')
 from LibMTL.weighting.MoCo_unizero import MoCo as GradCorrect
 # from LibMTL.weighting.CAGrad_unizero import CAGrad as GradCorrect
 
@@ -471,7 +471,40 @@ class UniZeroMTPolicy(UniZeroPolicy):
             self.grad_correct.init_param()  
             self.grad_correct.rep_grad = False
 
+        # 用于缓存上一帧的可塑性相关指标
+        self._prev_plasticity_metrics = dict(
+            dormant_ratio_encoder      = 0.0,
+            dormant_ratio_transformer  = 0.0,
+            dormant_ratio_head         = 0.0,
+            avg_weight_mag_encoder     = 0.0,
+            avg_weight_mag_transformer = 0.0,
+            avg_weight_mag_head        = 0.0,
+            e_rank_last_linear         = 0.0,
+            e_rank_sim_norm            = 0.0,
+        )
 
+
+    @staticmethod
+    def _is_zero(x: Union[float, torch.Tensor], eps: float = 1e-8) -> bool:
+        """
+        判断一个标量/0-D tensor 是否可视为 0
+        """
+        if isinstance(x, torch.Tensor):
+            return torch.all(torch.abs(x) < eps).item()
+        return abs(x) < eps
+
+    def _retain_prev_if_zero(self, name: str,
+                             value: Union[float, torch.Tensor]) -> Union[float, torch.Tensor]:
+        """
+        若 value≈0 则返回上一帧缓存值，否则更新缓存并返回当前值
+        """
+        if self._is_zero(value):
+            # 直接返回上一次的值（可能是 float，也可能是 tensor）
+            return self._prev_plasticity_metrics[name]
+        else:
+            # 更新缓存并返回当前值
+            self._prev_plasticity_metrics[name] = value
+            return value
 
 
     #@profile
@@ -633,14 +666,40 @@ class UniZeroMTPolicy(UniZeroPolicy):
             # ============ for value priority  ============ 
 
             # 关于网络可塑性的指标
-            dormant_ratio_encoder = intermediate_losses['dormant_ratio_encoder']
-            dormant_ratio_transformer = intermediate_losses['dormant_ratio_transformer']
-            dormant_ratio_head = intermediate_losses['dormant_ratio_head']
-            avg_weight_mag_encoder = intermediate_losses['avg_weight_mag_encoder']
-            avg_weight_mag_transformer = intermediate_losses['avg_weight_mag_transformer']
-            avg_weight_mag_head = intermediate_losses['avg_weight_mag_head']
-            e_rank_last_linear = intermediate_losses['e_rank_last_linear'] 
-            e_rank_sim_norm = intermediate_losses['e_rank_sim_norm']
+            # dormant_ratio_encoder = intermediate_losses['dormant_ratio_encoder']
+            # dormant_ratio_transformer = intermediate_losses['dormant_ratio_transformer']
+            # dormant_ratio_head = intermediate_losses['dormant_ratio_head']
+            # avg_weight_mag_encoder = intermediate_losses['avg_weight_mag_encoder']
+            # avg_weight_mag_transformer = intermediate_losses['avg_weight_mag_transformer']
+            # avg_weight_mag_head = intermediate_losses['avg_weight_mag_head']
+            # e_rank_last_linear = intermediate_losses['e_rank_last_linear'] 
+            # e_rank_sim_norm = intermediate_losses['e_rank_sim_norm']
+
+            dormant_ratio_encoder  = self._retain_prev_if_zero(
+                                'dormant_ratio_encoder',
+                                            intermediate_losses['dormant_ratio_encoder'])
+            dormant_ratio_transformer  = self._retain_prev_if_zero(
+                                            'dormant_ratio_transformer',
+                                            intermediate_losses['dormant_ratio_transformer'])
+            dormant_ratio_head         = self._retain_prev_if_zero(
+                                            'dormant_ratio_head',
+                                            intermediate_losses['dormant_ratio_head'])
+            avg_weight_mag_encoder     = self._retain_prev_if_zero(
+                                            'avg_weight_mag_encoder',
+                                            intermediate_losses['avg_weight_mag_encoder'])
+            avg_weight_mag_transformer = self._retain_prev_if_zero(
+                                            'avg_weight_mag_transformer',
+                                            intermediate_losses['avg_weight_mag_transformer'])
+            avg_weight_mag_head        = self._retain_prev_if_zero(
+                                            'avg_weight_mag_head',
+                                            intermediate_losses['avg_weight_mag_head'])
+            e_rank_last_linear         = self._retain_prev_if_zero(
+                                            'e_rank_last_linear',
+                                            intermediate_losses['e_rank_last_linear'])
+            e_rank_sim_norm            = self._retain_prev_if_zero(
+                                            'e_rank_sim_norm',
+                                            intermediate_losses['e_rank_sim_norm'])
+
             
             obs_loss_multi_task.append(obs_loss)
             reward_loss_multi_task.append(reward_loss)

zoo/atari/config/atari_unizero_multitask_segment_ddp_config_debug.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     Overview:
         This script should be executed with <nproc_per_node> GPUs.
         Run the following command to launch the script:
-        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29503 ./zoo/atari/config/atari_unizero_multitask_segment_ddp_config_debug.py 2>&1 | tee ./log/uz_mt_atari8_debug.log
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29503 ./zoo/atari/config/atari_unizero_multitask_segment_ddp_config_debug.py 2>&1 | tee ./log/20250518/uz_mt_atari8_debug.log
 
 
         torchrun --nproc_per_node=8 ./zoo/atari/config/atari_unizero_multitask_segment_8games_ddp_config.py

zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py
@@ -20,11 +20,13 @@ def create_config(env_id, observation_shape_list, action_space_size_list, collec
     task_name = env_id.split('-')[1]
 
     if domain_name == "pendulum":
-        frame_skip=8
-    else:
-        # frame_skip=2
-        frame_skip=8
+        # frame_skip=8
+        frame_skip=4
 
+    else:
+        # frame_skip=2 # orig
+        # frame_skip=8
+        frame_skip=4
 
     return EasyDict(dict(
         env=dict(
@@ -68,24 +70,28 @@ def create_config(env_id, observation_shape_list, action_space_size_list, collec
                 num_of_sampled_actions=20,
                 model_type='mlp',
                 world_model_cfg=dict(
-                    final_norm_option_in_obs_head='LayerNorm',
-                    final_norm_option_in_encoder='LayerNorm',
-                    predict_latent_loss_type='mse', # TODO: for latent state layer_norm
+                    # final_norm_option_in_obs_head='LayerNorm',
+                    # final_norm_option_in_encoder='LayerNorm',
+                    # predict_latent_loss_type='mse', # TODO: for latent state layer_norm
                     
+                    final_norm_option_in_obs_head='SimNorm',
+                    final_norm_option_in_encoder='SimNorm',
+                    predict_latent_loss_type='group_kl', # TODO: only for latent state sim_norm
+
                     share_head=False, # TODO
                     use_shared_projection=False,
 
-                    # analysis_dormant_ratio_weight_rank=True, # TODO
+                    # analysis_dormant_ratio_weight_rank=True, # TODO 按照atari unizeor_mt的更新一下 ========
                     analysis_dormant_ratio_weight_rank=False, # TODO
                     analysis_dormant_ratio_interval=100,
                     # analysis_dormant_ratio_interval=20,
                     
-                    task_embed_option=None,   # ==============TODO: none ==============
-                    use_task_embed=False, # ==============TODO==============
+                    # task_embed_option=None,   # ==============TODO: none ==============
+                    # use_task_embed=False, # ==============TODO==============
 
-                    # task_embed_option='concat_task_embed',   # ==============TODO: none ==============
-                    # use_task_embed=True, # ==============TODO==============
-                    # task_embed_dim=128,
+                    task_embed_option='concat_task_embed',   # ==============TODO: none ==============
+                    use_task_embed=True, # ==============TODO==============
+                    task_embed_dim=128,
 
                     observation_shape_list=observation_shape_list,
                     action_space_size_list=action_space_size_list,
@@ -105,12 +111,11 @@ def create_config(env_id, observation_shape_list, action_space_size_list, collec
                     context_length=2 * infer_context_length,
                     device='cuda',
                     # num_layers=1, # TODO: debug config
-                    num_layers=8,
+                    num_layers=8, # TODO: ==========
                     num_heads=24,
                     embed_dim=768,
                     env_num=max(collector_env_num, evaluator_env_num),
                     task_num=len(env_id_list),
-
 
                     use_normal_head=True,
                     use_softmoe_head=False,
@@ -154,7 +159,7 @@ def create_config(env_id, observation_shape_list, action_space_size_list, collec
             model_path=None,
             num_unroll_steps=num_unroll_steps,
             # update_per_collect=3,  # TODO: debug config
-            update_per_collect=200,  # TODO: 8*100*0.25=200
+            update_per_collect=200,  # TODO: 8*100*0.25=200 replay_ratio=0.25
             replay_ratio=reanalyze_ratio,
             batch_size=batch_size,
             optim_type='AdamW',
@@ -201,8 +206,11 @@ def generate_configs(env_id_list: List[str],
                     total_batch_size: int):
     configs = []
 
+    exp_name_prefix = f'data_suz_dmc_mt_20250522/dmc_{len(env_id_list)}tasks_frameskip4_simnorm-kl_nlayer8_trans-moe8_brf{buffer_reanalyze_freq}_seed{seed}/'
+    # exp_name_prefix = f'data_suz_dmc_mt_20250522/dmc_{len(env_id_list)}tasks_frameskip4_ln-mse_nlayer8_trans-moe8_brf{buffer_reanalyze_freq}_seed{seed}/'
+
     # exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250509/dmc_{len(env_id_list)}tasks_orig_nlayer8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
-    exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250508/dmc_{len(env_id_list)}tasks_frameskip8_nlayer8_trans-moe8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
+
     # exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250508/dmc_{len(env_id_list)}tasks_nlayer8_takembed128_trans-moe8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
 
     # exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250507/dmc_{len(env_id_list)}tasks_notaskembed_nlayer8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
@@ -260,8 +268,19 @@ if __name__ == "__main__":
     Overview:
         This script should be executed with <nproc_per_node> GPUs.
         Run the following command to launch the script:
-        # python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /fs-computility/ai-shen/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee ./log/uz_mt_dmc18_taskembed128-moe8_20250508.log
-        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /fs-computility/ai-shen/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee ./log/uz_mt_dmc18_moe8_20250509.log
+
+        =========== oss dmc18 =========================
+        cd /oss/niuyazhe/puyuan/data/data_lz_202505/
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /cpfs04/user/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee /cpfs04/user/puyuan/code/LightZero/log/20250522_oss/uz_mt_dmc18_frameskip4_ln-mse_nlayer8_trans-moe8.log
+
+        =========== cpfs dmc18 =========================
+        cd /cpfs04/user/puyuan/code/LightZero/
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /cpfs04/user/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee /cpfs04/user/puyuan/code/LightZero/log/20250522_cpfs/uz_mt_dmc18_frameskip4_simnorm-kl_nlayer8_trans-moe8.log
+
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /cpfs04/user/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee /cpfs04/user/puyuan/code/LightZero/log/20250522_cpfs/uz_mt_dmc18_frameskip4_ln-mse_nlayer8_trans-moe8.log
+
+
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /cpfs04/user/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee ./log/20250518/uz_mt_dmc18_moe8.log
 
         torchrun --nproc_per_node=8 ./zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py
     """
@@ -353,10 +372,10 @@ if __name__ == "__main__":
     n_episode = 8
     evaluator_env_num = 3
     num_simulations = 50
-    max_env_step = int(4e5)
+    max_env_step = int(4e5) # frameskip=8
     reanalyze_ratio = 0.0
 
-    # nlayer=8
+    # nlayer=4/8
     total_batch_size = 512
     batch_size = [int(min(64, total_batch_size / len(env_id_list))) for _ in range(len(env_id_list))]
 
@@ -381,28 +400,29 @@ if __name__ == "__main__":
     # batch_size = [2 for _ in range(len(env_id_list))]
     # =======================================
 
-    seed = 0  # You can iterate over multiple seeds if needed
+    for seed in [0,1]:
+        # You can iterate over multiple seeds if needed
 
-    configs = generate_configs(
-        env_id_list=env_id_list,
-        collector_env_num=collector_env_num,
-        n_episode=n_episode,
-        evaluator_env_num=evaluator_env_num,
-        num_simulations=num_simulations,
-        reanalyze_ratio=reanalyze_ratio,
-        batch_size=batch_size,
-        num_unroll_steps=num_unroll_steps,
-        infer_context_length=infer_context_length,
-        norm_type=norm_type,
-        seed=seed,
-        buffer_reanalyze_freq=buffer_reanalyze_freq,
-        reanalyze_batch_size=reanalyze_batch_size,
-        reanalyze_partition=reanalyze_partition,
-        num_segments=num_segments,
-        total_batch_size=total_batch_size,
-    )
+        configs = generate_configs(
+            env_id_list=env_id_list,
+            collector_env_num=collector_env_num,
+            n_episode=n_episode,
+            evaluator_env_num=evaluator_env_num,
+            num_simulations=num_simulations,
+            reanalyze_ratio=reanalyze_ratio,
+            batch_size=batch_size,
+            num_unroll_steps=num_unroll_steps,
+            infer_context_length=infer_context_length,
+            norm_type=norm_type,
+            seed=seed,
+            buffer_reanalyze_freq=buffer_reanalyze_freq,
+            reanalyze_batch_size=reanalyze_batch_size,
+            reanalyze_partition=reanalyze_partition,
+            num_segments=num_segments,
+            total_batch_size=total_batch_size,
+        )
 
-    with DDPContext():
-        train_unizero_multitask_segment_ddp(configs, seed=seed, max_env_step=max_env_step, benchmark_name= "dmc" )
-        # 如果只想训练部分任务，可以修改 configs，例如:
-        # train_unizero_multitask_segment_ddp(configs[:4], seed=seed, max_env_step=max_env_step)
+        with DDPContext():
+            train_unizero_multitask_segment_ddp(configs, seed=seed, max_env_step=max_env_step, benchmark_name= "dmc" )
+            # 如果只想训练部分任务，可以修改 configs，例如:
+            # train_unizero_multitask_segment_ddp(configs[:4], seed=seed, max_env_step=max_env_step)

zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config_debug.py
@@ -192,7 +192,7 @@ def generate_configs(env_id_list: List[str],
                     total_batch_size: int):
     configs = []
 
-    exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250508/dmc_{len(env_id_list)}tasks_orig_nlayer8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
+    exp_name_prefix = f'data_suz_dmc_mt_20250522/dmc_{len(env_id_list)}tasks_orig_nlayer8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
     # exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250508/dmc_{len(env_id_list)}tasks_nlayer8_takembed128_trans-moe8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
 
     # exp_name_prefix = f'data_lz/data_suz_dmc_mt_20250507/dmc_{len(env_id_list)}tasks_notaskembed_nlayer8_not-share-head_brf{buffer_reanalyze_freq}_seed{seed}/'
@@ -250,6 +250,11 @@ if __name__ == "__main__":
     Overview:
         This script should be executed with <nproc_per_node> GPUs.
         Run the following command to launch the script:
+                =========== oss atari26 =========================
+        cd /oss/niuyazhe/puyuan/data/data_lz_202505/
+        python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /cpfs04/user/puyuan/code/LightZero/zoo/atari/config/atari_unizero_multitask_segment_ddp_config.py 2>&1 | tee /cpfs04/user/puyuan/code/LightZero/log/20250522_oss/uz_mt_atari26_orig_nlayer8_seed01.log
+
+
         python -m torch.distributed.launch --nproc_per_node=8 --master_port=29502 /fs-computility/ai-shen/puyuan/code/LightZero/zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py 2>&1 | tee ./log/uz_mt_dmc18_orig_20250508.log
         torchrun --nproc_per_node=8 ./zoo/dmc2gym/config/dmc2gym_state_suz_multitask_ddp_config.py
     """