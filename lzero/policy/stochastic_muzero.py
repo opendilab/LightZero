36fd7206a348922893681f95dee74bc8cde00502
Eric Delabrouille <91535974+Firerozes@users.noreply.github.com>
2025-06-23 06:07:57 -0400

    fix(fir): fix timestep and non-text-based games compatibility for muzero (#372)
    fix timestep and non-text-based games for muzero

--- lzero/policy/stochastic_muzero.py
+++ lzero/policy/stochastic_muzero.py
@@ -580,6 +580,7 @@ class StochasticMuZeroPolicy(MuZeroPolicy):
             to_play: List = [-1],
             epsilon: float = 0.25,
             ready_env_id: np.array = None,
+            **kwargs,
     ) -> Dict:
         """
         Overview:
@@ -673,7 +674,7 @@ class StochasticMuZeroPolicy(MuZeroPolicy):
         else:
             self._mcts_eval = MCTSPtree(self._cfg)
 
-    def _forward_eval(self, data: torch.Tensor, action_mask: list, to_play: List = [-1], ready_env_id: np.array = None,) -> Dict:
+    def _forward_eval(self, data: torch.Tensor, action_mask: list, to_play: List = [-1], ready_env_id: np.array = None, **kwargs) -> Dict:
         """
         Overview:
             The forward function for evaluating the current policy in eval mode. Use model to execute MCTS search. \
